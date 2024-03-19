17e5f5f6d594abc41f07e71bc1a10e38f96abe60
Julian <julianthant@gmail.com>
2024-03-19 05:01:22 -0700

    Add new UI icons

---
+++ src/components/ui/Icons.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+
+interface ChevronDownProps {
+  fill: string;
+  size?: number;
+  height?: number;
+  width?: number;
+  [key: string]: any;
+}
+
+export const ChevronDown = ({
+  fill,
+  size,
+  height,
+  width,
+  ...props
+}: ChevronDownProps) => {
+  return (
+    <svg
+      fill="none"
+      height={size || height || 24}
+      viewBox="0 0 24 24"
+      width={size || width || 24}
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="m19.92 8.95-6.52 6.52c-.77.77-2.03.77-2.8 0L4.08 8.95"
+        stroke={fill}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit={10}
+        strokeWidth={1.5}
+      />
+    </svg>
+  );
+};
+
+interface SearchIconProps {
+  size?: number;
+  height?: number;
+  width?: number;
+  strokeWidth?: number;
+  [key: string]: any;
+}
+
+export const SearchIcon = ({
+  size = 24,
+  strokeWidth = 1.5,
+  width,
+  height,
+  ...props
+}: SearchIconProps) => {
+  return (
+    <svg
+      aria-hidden="true"
+      fill="none"
+      focusable="false"
+      height={height || size}
+      role="presentation"
+      viewBox="0 0 24 24"
+      width={width || size}
+      {...props}
+    >
+      <path
+        d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={strokeWidth}
+      />
+      <path
+        d="M22 22L20 20"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={strokeWidth}
+      />
+    </svg>
+  );
+};
