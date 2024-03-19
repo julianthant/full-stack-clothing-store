c8aa85d9ccc2dcb7f4c4448ef821ee7e1a3722d5
Julian <julianthant@gmail.com>
2024-03-19 05:01:38 -0700

    Add authentication configuration file

---
+++ src/lib/authentication/auth.config.ts
@@ -0,0 +1,39 @@
+import credentials from 'next-auth/providers/credentials';
+import google from 'next-auth/providers/google';
+import facebook from 'next-auth/providers/facebook';
+import bcrypt from 'bcryptjs';
+
+import type { NextAuthConfig } from 'next-auth';
+import { LoginSchema } from '@/schemas';
+import { getUserByEmail } from '@/data/user';
+
+export default {
+  providers: [
+    credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null;
+
+          const passwordMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordMatch) return user;
+        }
+
+        return null;
+      },
+    }),
+    google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    facebook({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    }),
+  ],
+} satisfies NextAuthConfig;
