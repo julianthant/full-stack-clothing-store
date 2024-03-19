9614dd0de90daedc8646f37c124992d53e9f310e
Julian <julianthant@gmail.com>
2024-03-19 05:00:57 -0700

    Add login and register schemas

---
+++ src/schemas/index.ts
@@ -0,0 +1,11 @@
+import * as z from 'zod';
+
+export const LoginSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+export const RegisterSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
