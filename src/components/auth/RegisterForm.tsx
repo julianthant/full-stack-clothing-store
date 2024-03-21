de6ebe870acc41affbd264d2913486955be2f41f
Julian <julianthant@gmail.com>
2024-03-21 02:41:51 -0700

    Add RegisterForm component

---
+++ src/components/auth/RegisterForm.tsx
@@ -0,0 +1,131 @@
+'use client';
+
+import * as React from 'react';
+
+import { cn } from '@/lib/utils';
+import { Icons } from '../ui/Icons';
+import { Button } from '../ui/button';
+import { Input } from '@nextui-org/react';
+import { Label } from '../ui/label';
+import {
+  GoogleIcon,
+  FacebookIcon,
+  EyeFilledIcon,
+  EyeSlashFilledIcon,
+} from '../ui/Icons';
+
+interface RegisterFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function RegisterForm({ className, ...props }: RegisterFormProps) {
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [isVisible, setIsVisible] = React.useState(false);
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
+
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }
+
+  return (
+    <div className={cn('grid gap-6', className)} {...props}>
+      <form onSubmit={onSubmit}>
+        <div className="grid gap-2">
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="email">
+              Email
+            </Label>
+            <Input
+              key="inside"
+              type="email"
+              variant="bordered"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              placeholder="name@example.com"
+              radius="sm"
+              disabled={isLoading}
+            />
+          </div>
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="password">
+              Password
+            </Label>
+            <Input
+              key="inside"
+              type={isVisible ? 'text' : 'password'}
+              variant="bordered"
+              autoCapitalize="none"
+              autoComplete="password"
+              autoCorrect="off"
+              disabled={isLoading}
+              placeholder="password"
+              radius="sm"
+              endContent={
+                <button
+                  className="focus:outline-none"
+                  type="button"
+                  onClick={toggleVisibility}
+                >
+                  {isVisible ? (
+                    <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                  ) : (
+                    <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                  )}
+                </button>
+              }
+            />
+          </div>
+          <Button disabled={isLoading} className="mt-1">
+            {isLoading && (
+              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+            )}
+            Sign Up with Email
+          </Button>
+        </div>
+      </form>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">
+            Or continue with
+          </span>
+        </div>
+      </div>
+      <div className="flex gap-4">
+        <Button
+          variant="outline"
+          type="button"
+          disabled={isLoading}
+          className="w-full"
+        >
+          {isLoading ? (
+            <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+          ) : (
+            <GoogleIcon />
+          )}{' '}
+          Google
+        </Button>
+        <Button
+          variant="outline"
+          type="button"
+          disabled={isLoading}
+          className="w-full"
+        >
+          {isLoading ? (
+            <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+          ) : (
+            <FacebookIcon />
+          )}{' '}
+          Facebook
+        </Button>
+      </div>
+    </div>
+  );
+}
