944c5b6cbb5beef6785762d123456b977e6e7cf9
Julian <julianthant@gmail.com>
2024-03-21 02:41:46 -0700

    Add RegisterForm component and update page layout

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,9 +1,30 @@
+import { RegisterForm } from '@/components/auth/RegisterForm';
 import { FC } from 'react';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
-  return <div>page</div>;
+  return (
+    <>
+      <Link
+        href="/auth/login"
+        className={cn('absolute right-4 top-4 md:right-8 md:top-8')}
+      >
+        Login
+      </Link>
+      <div className="flex flex-col space-y-2 text-center">
+        <h1 className="text-2xl font-semibold tracking-tight">
+          Create an account
+        </h1>
+        <p className="text-sm text-muted-foreground">
+          Enter your email below to create your account
+        </p>
+      </div>
+      <RegisterForm />
+    </>
+  );
 };
 
 export default page;
