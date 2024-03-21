c46c1a9b12d41a5e509f20683667bb2a50382ad3
Julian <julianthant@gmail.com>
2024-03-21 02:41:39 -0700

    Add Navbar component to Providers and conditionally render it based on the pathname

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,17 @@
 // app/providers.tsx
 'use client';
 
+import { usePathname } from 'next/navigation';
+
 import { NextUIProvider } from '@nextui-org/react';
+import Navbar from '@/components/ui/Navbar';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  const pathname = usePathname();
+  return (
+    <NextUIProvider>
+      {!pathname.includes('/auth') && <Navbar />}
+      {children}
+    </NextUIProvider>
+  );
 }
