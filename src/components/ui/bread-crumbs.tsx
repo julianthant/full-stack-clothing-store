ad0faea5ae7166d766b5343dc3955ccf78569249
Julian <julianthant@gmail.com>
2024-03-30 22:26:06 -0700

    Add BreadCrumbs component to UI

---
+++ src/components/ui/bread-crumbs.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { Breadcrumbs, BreadcrumbItem } from '@nextui-org/react';
+
+export const BreadCrumbs = () => {
+  const pathname = usePathname();
+  const linkTabs = pathname.split('/').filter((tab) => tab !== '');
+
+  const getBreadCrumbs = () => {
+    let path = '';
+
+    return linkTabs.map((tab, i) => {
+      path += `/${tab}`;
+
+      return (
+        <BreadcrumbItem key={i + 1} href={path} className="capitalize">
+          {tab}
+        </BreadcrumbItem>
+      );
+    });
+  };
+
+  return (
+    <Breadcrumbs>
+      {linkTabs.length > 0 && (
+        <BreadcrumbItem key={0} href="/">
+          Home
+        </BreadcrumbItem>
+      )}
+
+      {getBreadCrumbs()}
+    </Breadcrumbs>
+  );
+};
