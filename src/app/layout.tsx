424d266fad14aea3f98d97464b7273b77a279601
Julian <julianthant@gmail.com>
2024-03-31 02:12:31 -0700

    Remove 'newrelic' import from layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import { GeistSans } from 'geist/font/sans';
 import './globals.css';
 import { Providers } from './providers';
-import 'newrelic';
 
 export const metadata: Metadata = {
   title: 'Clothes.co',
