696f4f78ea2828131794e7054ef94bf3d4ffe81b
Julian <julianthant@gmail.com>
2024-03-30 22:25:04 -0700

    Update function to handle edge case

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,134 +1,322 @@
 'use client';
 
+import * as z from 'zod';
 import * as React from 'react';
 
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useSearchParams } from 'next/navigation';
+
+import { Login } from '@/actions/login';
+import { SignIn } from '@/actions/oauth-login';
+import { LoginSchema } from '@/schemas';
+
+import { FormError } from '../utils/FormError';
+import { FormSuccess } from '../utils/Form.Success';
+
 import { cn } from '@/lib/utils';
-import { Icons } from '../ui/Icons';
+import { Icons } from '../utils/Icons';
 import { Button } from '../ui/button';
 import { Input, Link } from '@nextui-org/react';
-import { Label } from '../ui/label';
+
 import {
-  GoogleIcon,
-  FacebookIcon,
-  EyeFilledIcon,
-  EyeSlashFilledIcon,
-} from '../ui/Icons';
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from '@/components/ui/input-otp';
 
 interface LoginFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function LoginForm({ className, ...props }: LoginFormProps) {
-  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [error, setError] = React.useState<string | undefined>('');
+  const [success, setSuccess] = React.useState<string | undefined>('');
+
   const [isVisible, setIsVisible] = React.useState(false);
+  const [isPending, startTransition] = React.useTransition();
+
+  const [showTwoFactor, setShowTwoFactor] = React.useState(false);
 
   const toggleVisibility = () => setIsVisible(!isVisible);
 
-  async function onSubmit(event: React.SyntheticEvent) {
-    event.preventDefault();
-    setIsLoading(true);
+  const searchParams = useSearchParams();
+  const urlError =
+    searchParams.get('error') === 'OAuthAccountNotLinked'
+      ? 'An account with the same email already exists!'
+      : '';
 
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 3000);
-  }
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    setError('');
+    setSuccess('');
+
+    startTransition(() => {
+      Login(values)
+        .then((data) => {
+          if (data?.error) {
+            form.reset();
+            setError(data.error);
+          }
+
+          if (data?.success) {
+            form.reset();
+            setSuccess(data.success);
+          }
+
+          if (data?.twoFactor) {
+            setShowTwoFactor(true);
+          }
+        })
+        .catch(() => setError('Something went wrong!'));
+    });
+  };
 
   return (
     <div className={cn('grid gap-6', className)} {...props}>
-      <form onSubmit={onSubmit}>
-        <div className="grid gap-2">
-          <div className="grid gap-1">
-            <Label className="sr-only" htmlFor="email">
-              Email
-            </Label>
-            <Input
-              key="inside"
-              type="email"
-              variant="bordered"
-              autoCapitalize="none"
-              autoComplete="email"
-              autoCorrect="off"
-              placeholder="name@example.com"
-              radius="sm"
-              disabled={isLoading}
-            />
-          </div>
-          <div className="grid gap-1">
-            <Label className="sr-only" htmlFor="password">
-              Password
-            </Label>
-            <Input
-              key="inside"
-              type={isVisible ? 'text' : 'password'}
-              variant="bordered"
-              autoCapitalize="none"
-              autoComplete="password"
-              autoCorrect="off"
-              disabled={isLoading}
-              placeholder="password"
-              radius="sm"
-              endContent={
-                <button
-                  className="focus:outline-none"
-                  type="button"
-                  onClick={toggleVisibility}
-                >
-                  {isVisible ? (
-                    <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                  ) : (
-                    <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                  )}
-                </button>
-              }
-            />
-          </div>
-          <Link
-            href="/auth/new-password"
-            size="sm"
-            className="text-blue-500 text-center ml-auto"
-          >
-            Forgot Password?
-          </Link>
-          <Button disabled={isLoading} className="mt-1">
-            {isLoading && (
-              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-            )}
-            Sign In with Email
-          </Button>
+      {showTwoFactor ? (
+        <Link
+          href="/auth/login"
+          className={cn('absolute right-4 top-4 md:right-8 md:top-8')}
+        >
+          Login
+        </Link>
+      ) : (
+        <Link
+          href="/auth/register"
+          className={cn('absolute right-4 top-4 md:right-8 md:top-8')}
+        >
+          Register
+        </Link>
+      )}
+
+      {showTwoFactor ? (
+        <div className="flex flex-col space-y-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Two Factor Authentication
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Enter your 2FA code to login
+          </p>
         </div>
-      </form>
+      ) : (
+        <div className="flex flex-col space-y-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">Login</h1>
+          <p className="text-sm text-muted-foreground">
+            Enter your email and password to login
+          </p>
+        </div>
+      )}
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          {showTwoFactor ? (
+            <div className="space-y-5">
+              <div className="flex items-center justify-center">
+                <FormField
+                  control={form.control}
+                  name="code"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="sr-only" htmlFor="code">
+                        OTP Code
+                      </FormLabel>
+
+                      <FormControl>
+                        <InputOTP {...field} disabled={isPending} maxLength={6}>
+                          <InputOTPGroup>
+                            <InputOTPSlot index={0} />
+                            <InputOTPSlot index={1} />
+                          </InputOTPGroup>
+                          <InputOTPSeparator />
+                          <InputOTPGroup>
+                            <InputOTPSlot index={2} />
+                            <InputOTPSlot index={3} />
+                          </InputOTPGroup>
+                          <InputOTPSeparator />
+                          <InputOTPGroup>
+                            <InputOTPSlot index={4} />
+                            <InputOTPSlot index={5} />
+                          </InputOTPGroup>
+                        </InputOTP>
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <FormError message={error} />
+
+              <Button
+                disabled={isPending}
+                className="mt-1 w-full"
+                type="submit"
+              >
+                {isPending && (
+                  <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                )}
+                Submit
+              </Button>
+            </div>
+          ) : (
+            <div className="grid">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="sr-only" htmlFor="email">
+                      Email
+                    </FormLabel>
+
+                    <FormControl>
+                      <Input
+                        key="inside"
+                        {...field}
+                        type="email"
+                        variant="bordered"
+                        autoCapitalize="none"
+                        autoComplete="email"
+                        autoCorrect="off"
+                        placeholder="name@example.com"
+                        radius="sm"
+                        disabled={isPending}
+                      />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="sr-only" htmlFor="password">
+                      Password
+                    </FormLabel>
+
+                    <FormControl>
+                      <Input
+                        key="inside"
+                        {...field}
+                        type={isVisible ? 'text' : 'password'}
+                        variant="bordered"
+                        autoCapitalize="none"
+                        autoComplete="Password"
+                        autoCorrect="off"
+                        disabled={isPending}
+                        placeholder="password"
+                        radius="sm"
+                        endContent={
+                          <button
+                            className="focus:outline-none"
+                            type="button"
+                            onClick={toggleVisibility}
+                          >
+                            {isVisible ? (
+                              <Icons.eyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                            ) : (
+                              <Icons.eyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                            )}
+                          </button>
+                        }
+                      />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormError message={error || urlError} />
+              <FormSuccess message={success} />
+
+              <Link
+                href="/auth/reset"
+                size="sm"
+                className="text-blue-700 text-center ml-auto mt-2 mb-3"
+                underline="hover"
+              >
+                Forgot Password?
+              </Link>
+
+              <Button disabled={isPending} className="mt-1" type="submit">
+                {isPending && (
+                  <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                )}
+                Sign In with Email
+              </Button>
+            </div>
+          )}
+        </form>
+      </Form>
+
       <div className="relative">
         <div className="absolute inset-0 flex items-center">
           <span className="w-full border-t" />
         </div>
+
         <div className="relative flex justify-center text-xs uppercase">
           <span className="bg-background px-2 text-muted-foreground">
             Or continue with
           </span>
         </div>
       </div>
+
       <div className="flex gap-4">
         <Button
           variant="outline"
           type="button"
-          disabled={isLoading}
+          disabled={isPending}
           className="w-full"
+          onClick={() => {
+            startTransition(() => {
+              SignIn('google');
+            });
+          }}
         >
-          {isLoading ? (
+          {isPending ? (
             <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
           ) : (
-            <GoogleIcon />
+            <Icons.google />
           )}{' '}
           Google
         </Button>
+
         <Button
           variant="outline"
           type="button"
-          disabled={isLoading}
+          disabled={isPending}
           className="w-full"
+          onClick={() => {
+            startTransition(() => {
+              SignIn('facebook');
+            });
+          }}
         >
-          {isLoading ? (
+          {isPending ? (
             <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
           ) : (
-            <FacebookIcon />
+            <Icons.facebook />
           )}{' '}
           Facebook
         </Button>
