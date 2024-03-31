9435305cc5b20e035e6c8b5409cef0fdc47604c4
Julian <julianthant@gmail.com>
2024-03-30 22:24:37 -0700

    Add reset password page component

---
+++ src/app/(main)/(dashboard)/settings/edit/reset-password/page.tsx
@@ -0,0 +1,10 @@
+import { PasswordReset } from '@/components/settings/editForms/PasswordReset';
+import { FC } from 'react';
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return <PasswordReset />;
+};
+
+export default page;
