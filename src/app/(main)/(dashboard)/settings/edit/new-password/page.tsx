92b1f61792d383c76c03d88b0f53502a6e6a6548
Julian <julianthant@gmail.com>
2024-03-30 22:24:28 -0700

    Add new password page component

---
+++ src/app/(main)/(dashboard)/settings/edit/new-password/page.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import { NewPassword } from '@/components/settings/editForms/NewPassword';
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return <NewPassword />;
+};
+
+export default page;
