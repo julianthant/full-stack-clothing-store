8eec866041c65c1119fdf0feecbbc99b2c9d1e75
Julian <julianthant@gmail.com>
2024-03-31 02:11:17 -0700

    Add newrelic import to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { GeistSans } from 'geist/font/sans';
 import './globals.css';
 import { Providers } from './providers';
+import 'newrelic';
 
 export const metadata: Metadata = {
   title: 'Clothes.co',
