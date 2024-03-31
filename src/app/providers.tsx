75fde28ce2bf7b4522f5e71e73160667b16fec17
Julian <julianthant@gmail.com>
2024-03-30 22:24:00 -0700

    Remove unused imports and navbar from Providers component

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,17 +1,8 @@
 // app/providers.tsx
 'use client';
 
-import { usePathname } from 'next/navigation';
-
 import { NextUIProvider } from '@nextui-org/react';
-import Navbar from '@/components/ui/Navbar';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-  return (
-    <NextUIProvider>
-      {!pathname.includes('/auth') && <Navbar />}
-      {children}
-    </NextUIProvider>
-  );
+  return <NextUIProvider>{children}</NextUIProvider>;
 }
