9165500af1976bcb885f8bd052a03cb465a35f7e
Julian <julianthant@gmail.com>
2024-03-30 22:24:26 -0700

    Add new page for editing name

---
+++ src/app/(main)/(dashboard)/settings/edit/new-name/page.tsx
@@ -0,0 +1,10 @@
+import { NameEdit } from '@/components/settings/editForms/NameEdit';
+import { FC } from 'react';
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return <NameEdit />;
+};
+
+export default page;
