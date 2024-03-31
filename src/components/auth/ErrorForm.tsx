f4b963c62e81d57b816d51b8c57479dca1aaa813
Julian <julianthant@gmail.com>
2024-03-30 22:25:02 -0700

    Add ErrorForm component

---
+++ src/components/auth/ErrorForm.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { FC } from 'react';
+import { Button } from '@/components/ui/button';
+
+import Link from 'next/link';
+
+interface ErrorFormProps {}
+
+const ErrorForm: FC<ErrorFormProps> = ({}) => {
+  return (
+    <>
+      <div className="flex flex-col space-y-2 text-center">
+        <h1 className="text-2xl font-semibold tracking-tight">Error</h1>
+        <p className="text-sm text-muted-foreground">Something went wrong</p>
+      </div>
+
+      <Button asChild>
+        <Link href="/auth/login">Back to Login</Link>
+      </Button>
+    </>
+  );
+};
+
+export default ErrorForm;
