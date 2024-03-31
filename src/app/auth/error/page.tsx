dd24e2453626d2655abf37b70d190348df5bd813
Julian <julianthant@gmail.com>
2024-03-30 22:25:28 -0700

    Add error page component

---
+++ src/app/auth/error/page.tsx
@@ -0,0 +1,10 @@
+import ErrorForm from '@/components/auth/ErrorForm';
+import { FC } from 'react';
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return <ErrorForm />;
+};
+
+export default page;
