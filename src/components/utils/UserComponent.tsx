cca09b16699e019dd012f90dad7676c20a9a2a87
Julian <julianthant@gmail.com>
2024-03-30 22:26:27 -0700

    Add UserComponent to display user information

---
+++ src/components/utils/UserComponent.tsx
@@ -0,0 +1,22 @@
+import { User } from '@nextui-org/user';
+
+import { useCurrentUser } from '@/hooks/useCurrentUser';
+
+export const UserComponent = () => {
+  const user = useCurrentUser();
+
+  return (
+    <User
+      name={user?.name}
+      description={user?.email}
+      classNames={{
+        name: 'text-default-600',
+        description: 'text-default-500',
+      }}
+      avatarProps={{
+        size: 'sm',
+        src: user?.image as string | undefined,
+      }}
+    />
+  );
+};
