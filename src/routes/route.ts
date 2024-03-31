052e0c43949ad6f942ffb4f2095444b6ff9b3c49
Julian <julianthant@gmail.com>
2024-03-31 01:45:54 -0700

    Update publicRoutes in route.ts

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -3,7 +3,12 @@
  * These routes do not require authentication
  * @type {string[]}
  */
-export const publicRoutes = ['/', '/auth/new-verification'];
+export const publicRoutes = [
+  '/',
+  '/auth/new-verification',
+  '/privacy',
+  '/terms',
+];
 
 /**
  * An array of routes that cannot be accessed
