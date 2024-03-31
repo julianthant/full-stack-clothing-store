39a7cd85039fa1f29cf6df968ffc9f4a0baf7e9c
Julian <julianthant@gmail.com>
2024-03-30 22:25:06 -0700

    Add EmailVerification component

---
+++ src/components/auth/NewVerification.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import { useCallback, useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { newVerification } from '@/actions/new-verification';
+
+import { Spinner } from '@nextui-org/react';
+import { Button } from '@/components/ui/button';
+import { FormError } from '@/components/utils/FormError';
+import { FormSuccess } from '@/components/utils/Form.Success';
+
+import Link from 'next/link';
+
+export const EmailVerification = () => {
+  const [error, setError] = useState<string | undefined>('');
+  const [success, setSuccess] = useState<string | undefined>('');
+
+  const searchParams = useSearchParams();
+  const token = searchParams.get('token');
+
+  const onSubmit = useCallback(() => {
+    if (success || error) return;
+
+    if (!token) {
+      setError('Token is missing!');
+      return;
+    }
+
+    newVerification(token)
+      .then((data) => {
+        setError(data.error);
+        setSuccess(data.success);
+      })
+      .catch(() => {
+        setError('Something went wrong!');
+      });
+  }, [token, success, error]);
+
+  useEffect(() => {
+    onSubmit();
+  }, [onSubmit]);
+
+  return (
+    <>
+      <div className="flex flex-col space-y-2 text-center">
+        <h1 className="text-2xl font-semibold tracking-tight">
+          Email Verifification
+        </h1>
+        <p className="text-sm text-muted-foreground">Verify your email</p>
+      </div>
+
+      {!error && !success && <Spinner color="primary" />}
+
+      <FormSuccess message={success} />
+
+      {!success && <FormError message={error} />}
+
+      <Button asChild>
+        <Link href="/auth/login">Back to Login</Link>
+      </Button>
+    </>
+  );
+};
