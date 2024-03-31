4b79d19395ec35a57fa0c540040143c04ea9a546
Julian <julianthant@gmail.com>
2024-03-30 22:24:24 -0700

    Add new email edit page

---
+++ src/app/(main)/(dashboard)/settings/edit/new-email/page.tsx
@@ -0,0 +1,10 @@
+import { EmailEdit } from '@/components/settings/editForms/EmailReset';
+import { FC } from 'react';
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return <EmailEdit />;
+};
+
+export default page;
