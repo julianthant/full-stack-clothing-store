80e0f8d7a2218f1ca44e4210e2c6c571a574a661
Julian <julianthant@gmail.com>
2024-03-30 22:25:43 -0700

    Add CountryEdit component

---
+++ src/components/settings/editForms/CountryEdit.tsx
@@ -0,0 +1,134 @@
+'use client';
+
+import * as z from 'zod';
+import * as React from 'react';
+
+import { NameSchema } from '@/schemas';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { ChangeCountry } from '@/actions/change-country';
+
+import { FormError } from '../../utils/FormError';
+import { FormSuccess } from '../../utils/Form.Success';
+
+import { cn } from '@/lib/utils';
+import { Icons } from '../../utils/Icons';
+
+import { Button } from '../../ui/button';
+import { useForm } from 'react-hook-form';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
+interface CountryEditProps extends React.HTMLAttributes<HTMLDivElement> {
+  countryNames: string[];
+}
+
+export function CountryEdit({
+  countryNames,
+  className,
+  ...props
+}: CountryEditProps) {
+  const [error, setError] = React.useState<string | undefined>('');
+  const [success, setSuccess] = React.useState<string | undefined>('');
+
+  const [isPending, startTransition] = React.useTransition();
+
+  const form = useForm<z.infer<typeof NameSchema>>({
+    resolver: zodResolver(NameSchema),
+    defaultValues: {
+      name: '',
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof NameSchema>) => {
+    setError('');
+    setSuccess('');
+
+    startTransition(() => {
+      ChangeCountry(values).then((data) => {
+        setError(data?.error);
+        setSuccess(data?.success);
+      });
+    });
+  };
+
+  return (
+    <div className={cn('grid gap-6 w-full', className)} {...props}>
+      <div className="flex flex-col space-y-2 text-center">
+        <h1 className="text-2xl font-semibold tracking-tight">
+          Change Country
+        </h1>
+        <p className="text-sm text-muted-foreground">
+          Choose your current country
+        </p>
+      </div>
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="grid gap-1">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="sr-only" htmlFor="email">
+                    Email
+                  </FormLabel>
+
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select your country" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>Countries</SelectLabel>
+                        {countryNames.sort().map((country) => (
+                          <SelectItem value={country} key={country}>
+                            {country}
+                          </SelectItem>
+                        ))}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormError message={error} />
+            <FormSuccess message={success} />
+
+            <Button disabled={isPending} className="mt-2" type="submit">
+              {isPending && (
+                <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+              )}
+              Select country
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+}
