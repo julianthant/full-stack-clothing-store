ba15e5f486025f7ce115882abb8906d71b991e9e
Julian <julianthant@gmail.com>
2024-03-30 22:24:17 -0700

    Add settings page component

---
+++ src/app/(main)/(dashboard)/settings/page.tsx
@@ -0,0 +1,10 @@
+import { SettingsComponent } from '@/components/settings/SettingsComponent';
+import { FC } from 'react';
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return <SettingsComponent />;
+};
+
+export default page;
