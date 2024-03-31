87d54f7bb4f19302dca5b4a6c8474b36d9ef68ca
Julian <julianthant@gmail.com>
2024-03-31 01:45:52 -0700

    Update email sender address

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendTwoFAEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: 'onoarding@resend.dev',
+    from: 'mail@julianhein.me',
     to: email,
     subject: 'Verify your email',
     html: `<p>Your 2FA code: ${token}</p>`,
@@ -15,7 +15,7 @@ export const sendVerficationEmail = async (email: string, token: string) => {
   const confirmLink = `${process.env.APP_URL}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: 'onoarding@resend.dev',
+    from: 'mail@julianhein.me',
     to: email,
     subject: 'Verify your email',
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
@@ -26,7 +26,7 @@ export const sendPasswordEmail = async (email: string, token: string) => {
   const resendLink = `${process.env.APP_URL}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: 'onoarding@resend.dev',
+    from: 'mail@julianhein.me',
     to: email,
     subject: 'Reset your password',
     html: `<p>Click <a href="${resendLink}">here</a> to reset your password.</p>`,
@@ -40,7 +40,7 @@ export const sendLoggedInPasswordEmail = async (
   const resendLink = `${process.env.APP_URL}/settings/edit/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: 'onoarding@resend.dev',
+    from: 'mail@julianhein.me',
     to: email,
     subject: 'Reset your password',
     html: `<p>Click <a href="${resendLink}">here</a> to reset your password.</p>`,
@@ -54,7 +54,7 @@ export const sendLoggedInVerficationEmail = async (
   const confirmLink = `${process.env.APP_URL}/settings/edit/verify-email?token=${token}`;
 
   await resend.emails.send({
-    from: 'onoarding@resend.dev',
+    from: 'mail@julianhein.me',
     to: email,
     subject: 'Verify your email',
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
