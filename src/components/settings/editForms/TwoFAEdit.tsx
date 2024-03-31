f4ff65124e9d7c1ab930fe4b721dd1b37ba72658
Julian <julianthant@gmail.com>
2024-03-30 22:26:01 -0700

    Add TwoFAEdit component

---
+++ src/components/settings/editForms/TwoFAEdit.tsx
@@ -0,0 +1,150 @@
+'use client';
+
+import * as z from 'zod';
+import * as React from 'react';
+
+import { TwoFASchema } from '@/schemas';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { FormError } from '../../utils/FormError';
+import { FormSuccess } from '../../utils/Form.Success';
+import { Validate2FACode } from '@/actions/activate-2fa';
+import { useSearchParams } from 'next/navigation';
+
+import { cn } from '@/lib/utils';
+import { Icons } from '../../utils/Icons';
+import { Button } from '../../ui/button';
+import { useForm } from 'react-hook-form';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from '@/components/ui/input-otp';
+
+interface TwoFAEditProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function TwoFAEdit({ className, ...props }: TwoFAEditProps) {
+  const [error, setError] = React.useState<string | undefined>('');
+  const [success, setSuccess] = React.useState<string | undefined>('');
+  const [formError, setFormError] = React.useState<string | undefined>('');
+
+  const [isPending, startTransition] = React.useTransition();
+
+  const searchParams = useSearchParams();
+  let formType = searchParams.get('type');
+
+  React.useEffect(() => {
+    if (formType !== 'activate' && formType !== 'deactivate') {
+      setFormError('Invalid form type');
+    }
+  }, [formType]);
+
+  const form = useForm<z.infer<typeof TwoFASchema>>({
+    resolver: zodResolver(TwoFASchema),
+    defaultValues: {
+      code: '',
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof TwoFASchema>) => {
+    setError('');
+    setSuccess('');
+
+    startTransition(() => {
+      Validate2FACode(values, formType as string).then((data) => {
+        setError(data?.error);
+        setSuccess(data?.success);
+      });
+    });
+  };
+
+  return (
+    <div className={cn('grid gap-6 w-full', className)} {...props}>
+      <div className="flex flex-col space-y-2 text-center">
+        <h1 className="text-2xl font-semibold tracking-tight">
+          Two Factor Authentication
+        </h1>
+        {formType === 'activate' && (
+          <p className="text-sm text-muted-foreground">
+            Enter the activation code in your email
+          </p>
+        )}
+        {formType === 'deactivate' && (
+          <p className="text-sm text-muted-foreground">
+            Enter the deactivation code in your email
+          </p>
+        )}
+      </div>
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="space-y-5">
+            <div className="flex items-center justify-center">
+              <FormField
+                control={form.control}
+                name="code"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="sr-only" htmlFor="code">
+                      OTP Code
+                    </FormLabel>
+
+                    <FormControl>
+                      <InputOTP
+                        {...field}
+                        disabled={isPending || !!formError}
+                        maxLength={6}
+                      >
+                        <InputOTPGroup>
+                          <InputOTPSlot index={0} />
+                          <InputOTPSlot index={1} />
+                        </InputOTPGroup>
+                        <InputOTPSeparator />
+                        <InputOTPGroup>
+                          <InputOTPSlot index={2} />
+                          <InputOTPSlot index={3} />
+                        </InputOTPGroup>
+                        <InputOTPSeparator />
+                        <InputOTPGroup>
+                          <InputOTPSlot index={4} />
+                          <InputOTPSlot index={5} />
+                        </InputOTPGroup>
+                      </InputOTP>
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <FormError message={error || formError} />
+            <FormSuccess message={success} />
+
+            <Button
+              disabled={isPending || !!formError}
+              className="mt-2 w-full"
+              type="submit"
+            >
+              {isPending && (
+                <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+              )}
+              {formType === 'activate' ? 'Activate' : 'Deactivate'}
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+}
