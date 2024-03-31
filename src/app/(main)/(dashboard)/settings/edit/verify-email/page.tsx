403ad56cd40617c6542f5f63983dd4b9f75248cc
Julian <julianthant@gmail.com>
2024-03-30 22:24:46 -0700

    Add verify email page component

---
+++ src/app/(main)/(dashboard)/settings/edit/verify-email/page.tsx
@@ -0,0 +1,10 @@
+import { NewEmailVerification } from '@/components/settings/editForms/NewEmail';
+import { FC } from 'react';
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return <NewEmailVerification />;
+};
+
+export default page;
