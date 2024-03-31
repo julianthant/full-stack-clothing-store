86b5e1fdb34b431d4fc811ddb812f48e93f64815
Julian <julianthant@gmail.com>
2024-03-30 22:24:38 -0700

    Add two-factor authentication page

---
+++ src/app/(main)/(dashboard)/settings/edit/two-factor-authentication/page.tsx
@@ -0,0 +1,10 @@
+import { TwoFAEdit } from '@/components/settings/editForms/TwoFAEdit';
+import { FC } from 'react';
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return <TwoFAEdit />;
+};
+
+export default page;
