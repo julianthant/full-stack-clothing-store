ab47366e823170f553b8f86a300c6f8483079daf
Julian <julianthant@gmail.com>
2024-03-30 22:26:08 -0700

    Remove unused Fonts component

--- src/components/ui/Fonts.tsx
+++
@@ -1,9 +0,0 @@
-import { Gabarito } from 'next/font/google';
-
-export const gabarito = Gabarito({
-  subsets: ['latin'],
-  weight: '900',
-  style: ['normal'],
-  display: 'swap',
-  adjustFontFallback: false,
-});
