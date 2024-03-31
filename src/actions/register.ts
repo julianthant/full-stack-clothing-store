6ca71e49a522714b97d1a2d964e3efb5dd2f8dd6
Julian <julianthant@gmail.com>
2024-03-30 22:23:40 -0700

    Add register action to handle user registration

---
+++ src/actions/register.ts
@@ -0,0 +1,41 @@
+'use server';
+
+import * as z from 'zod';
+import { db } from '@/database/db';
+import bcrypt from 'bcryptjs';
+
+import { RegisterSchema } from '@/schemas';
+import { getUserByEmail } from '@/data/user';
+import { generateVerificationToken } from '@/lib/token';
+import { sendVerficationEmail } from '@/lib/mail';
+
+export const Register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validatedFields = RegisterSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: 'Invalid fields!' };
+  }
+
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: 'Email already in use!' };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  const verificationToken = await generateVerificationToken(email);
+
+  await sendVerficationEmail(verificationToken.email, verificationToken.token);
+
+  return { success: 'Confirmation email sent!' };
+};
