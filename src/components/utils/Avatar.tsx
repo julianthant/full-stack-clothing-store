71281b1d107be0447bfdfdd347174f6f0ba3c778
Julian <julianthant@gmail.com>
2024-03-31 20:28:20 -0700

    Refactor AvatarFallback component to conditionally render user image

--- src/components/utils/Avatar.tsx
+++ src/components/utils/Avatar.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import { Avatar, AvatarIcon } from '@nextui-org/react';
 
-export const AvatarFallback = () => {
+interface AvatarFallbackProps {
+  userImage: string;
+}
+
+export const AvatarFallback = ({ userImage }: AvatarFallbackProps) => {
   return (
     <div className="flex items-center">
-      <Avatar
-        icon={<AvatarIcon />}
-        classNames={{
-          icon: 'text-black/80',
-        }}
-        size="sm"
-      />
+      {userImage ? (
+        <Avatar
+          icon={<AvatarIcon />}
+          classNames={{
+            icon: 'text-black/80',
+          }}
+          size="sm"
+        />
+      ) : (
+        <Avatar
+          src={userImage}
+          classNames={{
+            icon: 'text-black/80',
+          }}
+          size="sm"
+        />
+      )}
     </div>
   );
 };
