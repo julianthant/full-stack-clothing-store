7e959bf6364817dfae71af599540f7c5c84a2198
Julian <julianthant@gmail.com>
2024-04-03 01:09:13 -0700

    Add payment method page component

---
+++ src/app/(main)/(dashboard)/settings/edit/add-payment-method/page.tsx
@@ -0,0 +1,10 @@
+import { PaymentMethodCard } from '@/components/settings/account/PaymentMethodCard';
+import { FC } from 'react';
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return <PaymentMethodCard />;
+};
+
+export default page;
