d5860929d731cda5082582e3d9bad41ef85d7d29
Julian <julianthant@gmail.com>
2024-04-04 04:48:10 -0700

    Update width of NameEdit component

--- src/components/settings/editForms/NameEdit.tsx
+++ src/components/settings/editForms/NameEdit.tsx
@@ -53,7 +53,7 @@ export function NameEdit({ className, ...props }: NameEditProps) {
   };
 
   return (
-    <div className={cn('grid gap-6 w-full', className)} {...props}>
+    <div className={cn('grid gap-6 w-[300px]', className)} {...props}>
       <div className="flex flex-col space-y-2 text-center">
         <h1 className="text-2xl font-semibold tracking-tight">Change Name</h1>
         <p className="text-sm text-muted-foreground">Enter your new name</p>
