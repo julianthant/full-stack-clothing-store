7d0876d5b296eb977406b02d85c1a32667718146
Julian <julianthant@gmail.com>
2024-04-04 04:48:30 -0700

    Refactor AvatarFallback component to handle null or undefined userImage

--- src/components/utils/Avatar.tsx
+++ src/components/utils/Avatar.tsx
@@ -2,23 +2,17 @@ import React from 'react';
 import { Avatar, AvatarIcon } from '@nextui-org/react';
 
 interface AvatarFallbackProps {
-  userImage: string;
+  userImage: string | null | undefined;
 }
 
 export const AvatarFallback = ({ userImage }: AvatarFallbackProps) => {
   return (
     <div className="flex items-center">
-      {userImage ? (
-        <Avatar
-          icon={<AvatarIcon />}
-          classNames={{
-            icon: 'text-black/80',
-          }}
-          size="sm"
-        />
+      {!!userImage ? (
+        <Avatar src={userImage} size="sm" />
       ) : (
         <Avatar
-          src={userImage}
+          icon={<AvatarIcon />}
           classNames={{
             icon: 'text-black/80',
           }}
