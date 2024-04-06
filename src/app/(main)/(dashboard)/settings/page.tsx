7e3e29533a1ca75fb23840e5ee96c387972c85fe
Julian <julianthant@gmail.com>
2024-04-06 03:05:51 -0700

    Remove unused code and dependencies in settings/page.tsx

--- src/app/(main)/(dashboard)/settings/page.tsx
+++ src/app/(main)/(dashboard)/settings/page.tsx
@@ -1,12 +1,10 @@
 import { SettingsDashboard } from '@/components/settings/SettingsDashboard';
-import { getPaymentMethodsByUserId } from '@/data/payment-methods';
 import { currentUser } from '@/lib/server-auth';
 
 const page = async () => {
   const user = await currentUser();
-  const paymentMethods = await getPaymentMethodsByUserId(user?.id as string);
 
-  return <SettingsDashboard payment={paymentMethods} />;
+  return <SettingsDashboard />;
 };
 
 export default page;
