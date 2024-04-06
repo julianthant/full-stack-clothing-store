7964151eaacec2d4ab6e23b26821820861af5006
julianthant <julianthant@gmail.com>
2024-04-06 03:48:08 -0700

    Added modal for spinner

--- src/components/settings/account/PaymentComponent.tsx
+++ src/components/settings/account/PaymentComponent.tsx
@@ -15,7 +15,7 @@ import { toast, ToastContainer } from 'react-toastify';
 
 import { Icons } from '@/components/utils/Icons';
 import { Button } from '@/components/ui/button';
-import { Divider, Skeleton } from '@nextui-org/react';
+import { Divider, Skeleton, Spinner } from '@nextui-org/react';
 import { PlusCircle } from 'lucide-react';
 
 import {
@@ -25,6 +25,15 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from '@nextui-org/react';
 
 export const PaymentComponent = () => {
   const [selectedCard, setSelectedCard] = useState(0);
@@ -45,7 +54,9 @@ export const PaymentComponent = () => {
       }[]
     | null
   >();
+
   const [remove, setRemoved] = useState(false);
+  const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
 
   const user = useCurrentUser();
   const params = useSearchParams();
@@ -61,6 +72,7 @@ export const PaymentComponent = () => {
         const cards = await getPaymentMethodsByUserId(user?.id as string);
         setPaymentMethods(cards);
         setRemoved(false);
+        onClose;
         router.replace(pathname + '?menu=Account&subMenu=Payments');
 
         success === 'true' &&
@@ -94,126 +106,141 @@ export const PaymentComponent = () => {
   }, [success, user?.id, remove]);
 
   return (
-    <div className="sm:flex grid gap-4">
-      <Card className="h-min">
-        <ToastContainer pauseOnFocusLoss={false} pauseOnHover={false} />
-        <CardHeader>
-          <CardTitle>Wallet</CardTitle>
-          <CardDescription>Methods & Accounts</CardDescription>
-        </CardHeader>
-        <CardContent className="grid bg-foreground-100 p-0 rounded-b-lg">
-          {!!paymentMethods ? (
-            paymentMethods.map((card, index) => (
-              <div
-                key={index}
-                onClick={() => setSelectedCard(index)}
-                className={cn(
-                  'hover:cursor-pointer hover:bg-foreground-200',
-                  selectedCard === index && 'bg-foreground-200'
-                )}
-              >
-                <div className="relative flex w-[340px] items-center justify-start px-6 py-4 gap-3">
-                  <div className="relative">
-                    <Image
-                      src={BlackCard}
-                      className="max-w-[85px]"
-                      alt="Credit Card"
-                      width={95}
-                      height={60}
-                    />
-                    {card.default && (
-                      <>
-                        <div className="absolute bg-green-600 right-0 top-2 w-6 h-4" />
-                        <div className="absolute bg-green-600 right-5 top-2 w-6 h-4 -skew-x-[150deg]" />
-                        <p className="absolute right-[0.2rem] top-2 text-[0.65rem] text-center text-black">
-                          Default
-                        </p>
-                      </>
+    <>
+      <div className="sm:flex grid gap-4">
+        <Card className="h-min">
+          <ToastContainer pauseOnFocusLoss={false} pauseOnHover={false} />
+          <CardHeader>
+            <CardTitle>Wallet</CardTitle>
+            <CardDescription>Methods & Accounts</CardDescription>
+          </CardHeader>
+          <CardContent className="grid bg-foreground-100 p-0 rounded-b-lg">
+            {!!paymentMethods ? (
+              paymentMethods.map((card, index) => (
+                <div
+                  key={index}
+                  onClick={() => setSelectedCard(index)}
+                  className={cn(
+                    'hover:cursor-pointer hover:bg-foreground-200',
+                    selectedCard === index && 'bg-foreground-200'
+                  )}
+                >
+                  <div className="relative flex w-[340px] items-center justify-start px-6 py-4 gap-3">
+                    <div className="relative">
+                      <Image
+                        src={BlackCard}
+                        className="max-w-[85px]"
+                        alt="Credit Card"
+                        width={85}
+                        height={53.45}
+                      />
+                      {card.default && (
+                        <>
+                          <div className="absolute bg-green-600 right-0 top-2 w-6 h-4" />
+                          <div className="absolute bg-green-600 right-5 top-2 w-6 h-4 -skew-x-[150deg]" />
+                          <p className="absolute right-[0.2rem] top-2 text-[0.65rem] text-center text-black">
+                            Default
+                          </p>
+                        </>
+                      )}
+                    </div>
+
+                    <div className=" rounded-lg">
+                      <h3 className={`${roboto.className} font-bold text-sm`}>
+                        {card.bankName} {card.cardScheme} {card.cardType} CARD
+                      </h3>
+                      <p className="text-xs">
+                        Card ending in &#x2022;&#x2022;&#x2022;&#x2022;{' '}
+                        {card.lastFourNumbers}
+                      </p>
+                    </div>
+
+                    {index === selectedCard && (
+                      <div className="absolute bg-orange-500 w-1 h-full left-0" />
                     )}
                   </div>
 
-                  <div className=" rounded-lg">
-                    <h3 className={`${roboto.className} font-bold text-sm`}>
-                      {card.bankName} {card.cardScheme} {card.cardType} CARD
-                    </h3>
-                    <p className="text-xs">
-                      Card ending in &#x2022;&#x2022;&#x2022;&#x2022;{' '}
-                      {card.lastFourNumbers}
-                    </p>
+                  <Divider />
+                </div>
+              ))
+            ) : (
+              <>
+                <div className="px-6 py-4 w-[340px] flex items-center gap-3 justify-start">
+                  <div>
+                    <Skeleton className="flex rounded-md w-[85px] h-[53.45px]" />
                   </div>
-
-                  {index === selectedCard && (
-                    <div className="absolute bg-orange-500 w-1 h-full left-0" />
-                  )}
+                  <div className="w-full flex flex-col gap-2">
+                    <Skeleton className="h-4 w rounded-lg" />
+                    <Skeleton className="h-4 w-3/5 rounded-lg" />
+                  </div>
                 </div>
 
                 <Divider />
-              </div>
-            ))
-          ) : (
-            <>
-              <div className="px-6 py-4 w-[340px] flex items-center gap-3 justify-start">
-                <div>
-                  <Skeleton className="flex rounded-md w-[85px] h-[53.45px]" />
+
+                <div className="px-6 py-4 w-[340px] flex items-center gap-3 justify-start">
+                  <div>
+                    <Skeleton className="flex rounded-md w-[85px] h-[53.45px]" />
+                  </div>
+                  <div className="w-full flex flex-col gap-2">
+                    <Skeleton className="h-4 w rounded-lg" />
+                    <Skeleton className="h-4 w-3/5 rounded-lg" />
+                  </div>
                 </div>
-                <div className="w-full flex flex-col gap-2">
-                  <Skeleton className="h-4 w rounded-lg" />
-                  <Skeleton className="h-4 w-3/5 rounded-lg" />
+
+                <Divider />
+              </>
+            )}
+
+            <Link href="/settings/edit/add-payment-method">
+              <div className="flex hover:cursor-pointer hover:bg-foreground-200 bg-foreground-100 px-6 py-4 gap-3 rounded-b-lg">
+                <div className="border-2 border-dotted w-[95px] h-14 border-foreground-400 flex items-center justify-center">
+                  <PlusCircle size={24} className="text-foreground-400" />
+                </div>
+                <div className="flex items-center justify-center">
+                  <p className="text-sm text-teal-600">Add a payment method</p>
                 </div>
               </div>
+            </Link>
+          </CardContent>
+        </Card>
 
-              <Divider />
-
-              <div className="px-6 py-4 w-[340px] flex items-center gap-3 justify-start">
-                <div>
-                  <Skeleton className="flex rounded-md w-[85px] h-[53.45px]" />
+        {!!paymentMethods && paymentMethods?.length > 0 && (
+          <Card className="h-min">
+            <CardHeader>
+              <div className="flex justify-between pl-1">
+                <div className="grid gap-2">
+                  <CardTitle>
+                    {paymentMethods[selectedCard].cardHolder}&apos;s
+                  </CardTitle>
+                  <CardDescription className="w-48">
+                    {paymentMethods[selectedCard].cardScheme}{' '}
+                    {paymentMethods[selectedCard].cardType} CARD
+                  </CardDescription>
                 </div>
-                <div className="w-full flex flex-col gap-2">
-                  <Skeleton className="h-4 w rounded-lg" />
-                  <Skeleton className="h-4 w-3/5 rounded-lg" />
-                </div>
-              </div>
-
-              <Divider />
-            </>
-          )}
-
-          <Link href="/settings/edit/add-payment-method">
-            <div className="flex hover:cursor-pointer hover:bg-foreground-200 bg-foreground-100 px-6 py-4 gap-3 rounded-b-lg">
-              <div className="border-2 border-dotted w-[95px] h-14 border-foreground-400 flex items-center justify-center">
-                <PlusCircle size={24} className="text-foreground-400" />
-              </div>
-              <div className="flex items-center justify-center">
-                <p className="text-sm text-teal-600">Add a payment method</p>
-              </div>
-            </div>
-          </Link>
-        </CardContent>
-      </Card>
-
-      {!!paymentMethods && paymentMethods?.length > 0 && (
-        <Card className="h-min">
-          <CardHeader>
-            <div className="flex justify-between pl-1">
-              <div className="grid gap-2">
-                <CardTitle>
-                  {paymentMethods[selectedCard].cardHolder}&apos;s
-                </CardTitle>
-                <CardDescription className="w-48">
-                  {paymentMethods[selectedCard].cardScheme}{' '}
-                  {paymentMethods[selectedCard].cardType} CARD
-                </CardDescription>
-              </div>
-              <form
-                className="ml-auto h-min"
-                onSubmit={async (event) => {
-                  event.preventDefault();
-                  await RemovePaymentMethod(paymentMethods[selectedCard].id)
-                    .then(() => {
-                      setRemoved(true);
-                      toast.success(
-                        `Removed Card Ending in ${paymentMethods[selectedCard].lastFourNumbers}`,
-                        {
+                <form
+                  className="ml-auto h-min"
+                  onSubmit={async (event) => {
+                    event.preventDefault();
+                    await RemovePaymentMethod(paymentMethods[selectedCard].id)
+                      .then(() => {
+                        onOpen;
+                        setRemoved(true);
+                        toast.success(
+                          `Removed Card Ending in ${paymentMethods[selectedCard].lastFourNumbers}`,
+                          {
+                            position: 'top-center',
+                            autoClose: 5000,
+                            hideProgressBar: false,
+                            closeOnClick: true,
+                            pauseOnHover: false,
+                            draggable: false,
+                            progress: undefined,
+                            theme: 'colored',
+                          }
+                        );
+                      })
+                      .catch(() => {
+                        toast.error(`Unable to remove card!`, {
                           position: 'top-center',
                           autoClose: 5000,
                           hideProgressBar: false,
@@ -222,76 +249,70 @@ export const PaymentComponent = () => {
                           draggable: false,
                           progress: undefined,
                           theme: 'colored',
-                        }
-                      );
-                    })
-                    .catch((err) => {
-                      toast.error(`Unable to remove card! ${err}`, {
-                        position: 'top-center',
-                        autoClose: 5000,
-                        hideProgressBar: false,
-                        closeOnClick: true,
-                        pauseOnHover: false,
-                        draggable: false,
-                        progress: undefined,
-                        theme: 'colored',
+                        });
                       });
-                    });
-                }}
-              >
+                  }}
+                >
+                  <Button
+                    type="submit"
+                    variant={'link'}
+                    className="w-min p-0 mr-1 h-min text-red-500"
+                  >
+                    Remove
+                  </Button>
+                </form>
+              </div>
+            </CardHeader>
+
+            <CardContent className="grid gap-2">
+              <div className="relative font-bold text-white">
+                <Image
+                  src={BlackCard}
+                  alt={paymentMethods[selectedCard].cardHolder + "'s Card"}
+                  width={300}
+                  height={189}
+                />
+                <p className="absolute top-[20px] left-[30px]">
+                  {paymentMethods[selectedCard].bankName}
+                </p>
+                <p className="absolute top-[120px] left-[30px]">
+                  {paymentMethods[selectedCard].expiryMonth.padStart(2, '0')}/
+                  {paymentMethods[selectedCard].expiryYear}
+                </p>
+                <p className="absolute top-[145px] left-[30px]">
+                  {paymentMethods[selectedCard].cardHolder}
+                </p>
+                {paymentMethods[selectedCard].cardScheme === 'MASTERCARD' && (
+                  <Icons.masterCardLogo className="absolute top-[116px] right-[37px] w-20 h-20" />
+                )}
+                {paymentMethods[selectedCard].cardScheme === 'VISA' && (
+                  <Icons.visaLogo className="absolute top-[115px] right-[42px] w-20 h-20" />
+                )}
+              </div>
+
+              <div className="mr-auto">
                 <Button
-                  type="submit"
+                  asChild
+                  className="w-full p-0 ml-1 text-teal-600"
                   variant={'link'}
-                  className="w-min p-0 mr-1 h-min text-red-500"
                 >
-                  Remove
+                  <Link
+                    href={`/settings/edit/edit-payment-method?card-id=${paymentMethods[selectedCard].id}`}
+                  >
+                    Edit
+                  </Link>
                 </Button>
-              </form>
-            </div>
-          </CardHeader>
+              </div>
+            </CardContent>
+          </Card>
+        )}
+      </div>
 
-          <CardContent className="grid gap-2">
-            <div className="relative font-bold text-white">
-              <Image
-                src={BlackCard}
-                alt={paymentMethods[selectedCard].cardHolder + "'s Card"}
-                width={300}
-                height={189}
-              />
-              <p className="absolute top-[20px] left-[30px]">
-                {paymentMethods[selectedCard].bankName}
-              </p>
-              <p className="absolute top-[120px] left-[30px]">
-                {paymentMethods[selectedCard].expiryMonth.padStart(2, '0')}/
-                {paymentMethods[selectedCard].expiryYear}
-              </p>
-              <p className="absolute top-[145px] left-[30px]">
-                {paymentMethods[selectedCard].cardHolder}
-              </p>
-              {paymentMethods[selectedCard].cardScheme === 'MASTERCARD' && (
-                <Icons.masterCardLogo className="absolute top-[116px] right-[37px] w-20 h-20" />
-              )}
-              {paymentMethods[selectedCard].cardScheme === 'VISA' && (
-                <Icons.visaLogo className="absolute top-[115px] right-[42px] w-20 h-20" />
-              )}
-            </div>
-
-            <div className="mr-auto">
-              <Button
-                asChild
-                className="w-full p-0 ml-1 text-teal-600"
-                variant={'link'}
-              >
-                <Link
-                  href={`/settings/edit/edit-payment-method?card-id=${paymentMethods[selectedCard].id}`}
-                >
-                  Edit
-                </Link>
-              </Button>
-            </div>
-          </CardContent>
-        </Card>
-      )}
-    </div>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          <Spinner size="lg" />
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
