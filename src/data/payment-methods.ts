537717ead75749588d2edad2bf692ebd357ccf66
Julian <julianthant@gmail.com>
2024-04-06 03:06:13 -0700

    Refactor payment method retrieval actions for better code organization and error handling

--- src/data/payment-methods.ts
+++ src/data/payment-methods.ts
@@ -1,38 +1,13 @@
 import { db } from '@/database/db';
 
-export const getPaymentMethodsByUserId = async (userId: string) => {
-  try {
-    const paymentMethods = await db.payment.findMany({
-      where: { userId },
-    });
-
-    return paymentMethods;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-};
-
-export const getPaymentMethodById = async (id: string) => {
-  try {
-    const paymentMethod = await db.payment.findUnique({
-      where: { id },
-    });
-
-    return paymentMethod;
-  } catch {
-    return null;
-  }
-};
-
-export const getPaymentMethodByCardNumber = async (cardNumber: string) => {
+export const getDefaultPaymentMethodByUserId = async (userId: string) => {
   try {
     const paymentMethod = await db.payment.findFirst({
-      where: { cardNumber },
+      where: { userId, default: true },
     });
 
     return paymentMethod;
-  } catch {
+  } catch (error) {
     return null;
   }
 };
