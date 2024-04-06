6482fe9131d4e9254baee56652b61ced87a807bf
Julian <julianthant@gmail.com>
2024-04-06 03:05:54 -0700

    Refactor page.tsx to use PaymentEditForm component

--- src/app/(main)/(dashboard)/settings/edit/edit-payment-method/page.tsx
+++ src/app/(main)/(dashboard)/settings/edit/edit-payment-method/page.tsx
@@ -1,9 +1,14 @@
 import { FC } from 'react';
+import { PaymentEditForm } from '@/components/settings/account/forms/PaymentEditForm';
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
-  return <div>page</div>;
+  return (
+    <div>
+      <PaymentEditForm />
+    </div>
+  );
 };
 
 export default page;
