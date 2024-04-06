443927fb793e27db2492880d6177ebf14e55240b
Julian <julianthant@gmail.com>
2024-04-06 03:06:07 -0700

    Update payment method validation and error handling

--- src/components/settings/account/forms/PaymentEditForm.tsx
+++ src/components/settings/account/forms/PaymentEditForm.tsx
@@ -1,12 +1,12 @@
 'use client';
 
 import * as z from 'zod';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSearchParams, useRouter } from 'next/navigation';
 
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { cardAddSchema } from '@/schemas';
-import { AddPaymentMethod } from '@/actions/add-payment-method';
+import { CardEditSchema } from '@/schemas';
 
 import { FormError } from '@/components/utils/FormError';
 import { FormSuccess } from '@/components/utils/Form.Success';
@@ -15,7 +15,8 @@ import { Input } from '@/components/ui/Input';
 import { Label } from '@/components/ui/label';
 import { Icons } from '@/components/utils/Icons';
 import { Button } from '@/components/ui/button';
-import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import { Spinner } from '@nextui-org/react';
+import { Checkbox } from '@/components/ui/checkbox';
 
 import {
   Select,
@@ -34,261 +35,249 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { getPaymentMethodById } from '@/actions/get-payment-method';
+import { UpdatePaymentMethod } from '@/actions/update-payment-method';
 
 export function PaymentEditForm() {
   const [error, setError] = React.useState<string | undefined>('');
   const [success, setSuccess] = React.useState<string | undefined>('');
 
-  const [isPending, startTransition] = React.useTransition();
+  const router = useRouter();
+  const params = useSearchParams();
+  const id = params.get('card-id');
 
-  const form = useForm<z.infer<typeof cardAddSchema>>({
-    resolver: zodResolver(cardAddSchema),
-    defaultValues: {
-      paymentType: 'card',
-      cardHolder: '',
-      cardNumber: '',
-      expiryMonth: '',
-      expiryYear: '',
-      cvc: '',
-    },
+  const [card, setCard] = React.useState<
+    | {
+        id: string;
+        userId: string;
+        bankName: string;
+        cardType: string;
+        cardScheme: string;
+        cardHolder: string;
+        cardNumber: string;
+        lastFourNumbers: string;
+        expiryMonth: string;
+        expiryYear: string;
+        cvc: string;
+        default: boolean;
+      }
+    | null
+    | undefined
+  >();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const card = await getPaymentMethodById(id as string);
+        setCard(card);
+      } catch {
+        setError('Failed to fetch card details');
+      }
+    })();
+  }, [id]);
+
+  const form = useForm<z.infer<typeof CardEditSchema>>({
+    resolver: zodResolver(CardEditSchema),
   });
 
-  const onSubmit = (values: z.infer<typeof cardAddSchema>) => {
+  useEffect(() => {
+    if (!!card) {
+      form.reset({
+        cardHolder: card?.cardHolder,
+        expiryMonth: card?.expiryMonth,
+        expiryYear: card?.expiryYear,
+        defaultCard: card?.default,
+      });
+    }
+  }, [card, form]);
+
+  const [isPending, startTransition] = React.useTransition();
+
+  const onSubmit = (values: z.infer<typeof CardEditSchema>) => {
     setError('');
     setSuccess('');
 
     startTransition(() => {
-      AddPaymentMethod(values).then((data) => {
+      UpdatePaymentMethod(values, id as string).then((data) => {
         setError(data?.error);
         setSuccess(data?.success);
       });
     });
   };
 
+  useEffect(() => {
+    if (!!success) {
+      const successMessage = encodeURI('Payment method updated successfully');
+      router.push(
+        '/settings/?menu=Account&subMenu=Payments&success=true&message=' +
+          successMessage
+      );
+    }
+
+    if (!!error) {
+      const errorMessage = encodeURI('Unable to update payment method');
+      router.push(
+        '/settings/?menu=Account&subMenu=Payments&success=false&message=' +
+          errorMessage
+      );
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [success, error]);
+
   return (
-    <Card className="border-0 shadow-none p-0 w-[500px]">
+    <Card className="border-0 shadow-none p-0 w-full">
       <CardHeader>
-        <CardTitle>Payment Method</CardTitle>
-        <CardDescription>
-          Add a new payment method to your account.
-        </CardDescription>
+        <div className="flex flex-col space-y-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Edit Payment Method
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Update your details below
+          </p>
+        </div>
       </CardHeader>
       <CardContent className="grid gap-6">
-        <Form {...form}>
-          <form className="space-y-3" onSubmit={form.handleSubmit(onSubmit)}>
-            <FormField
-              control={form.control}
-              name="paymentType"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="sr-only" htmlFor="name">
-                    Payment Type
-                  </FormLabel>
-
-                  <RadioGroup
-                    onValueChange={field.onChange}
-                    className="grid grid-cols-3 gap-4"
-                    disabled={isPending}
-                    defaultValue="card"
-                  >
-                    <div>
-                      <RadioGroupItem
-                        value="card"
-                        id="card"
-                        className="peer sr-only"
-                      />
-                      <Label
-                        htmlFor="card"
-                        className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          className="mb-3 h-6 w-6"
-                        >
-                          <rect width="20" height="14" x="2" y="5" rx="2" />
-                          <path d="M2 10h20" />
-                        </svg>
-                        Card
-                      </Label>
-                    </div>
-                    <div>
-                      <RadioGroupItem
-                        value="paypal"
-                        id="paypal"
-                        className="peer sr-only"
-                      />
-                      <Label
-                        htmlFor="paypal"
-                        className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
-                      >
-                        <Icons.paypal className="mb-3 h-6 w-6" />
-                        Paypal
-                      </Label>
-                    </div>
-                    <div>
-                      <RadioGroupItem
-                        value="apple"
-                        id="apple"
-                        className="peer sr-only"
-                      />
-                      <Label
-                        htmlFor="apple"
-                        className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
-                      >
-                        <Icons.apple className="mb-3 h-6 w-6" />
-                        Apple
-                      </Label>
-                    </div>
-                  </RadioGroup>
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="cardHolder"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel htmlFor="name">Name</FormLabel>
-                  <Input
-                    id="name"
-                    placeholder="Name on Card"
-                    onChange={field.onChange}
-                    disabled={isPending}
-                  />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="cardNumber"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel htmlFor="number">Card Number</FormLabel>
-                  <Input
-                    id="number"
-                    placeholder=""
-                    onChange={field.onChange}
-                    disabled={isPending}
-                  />
-                </FormItem>
-              )}
-            />
-
-            <div className="grid grid-cols-3 gap-4">
+        {!!card && (
+          <Form {...form}>
+            <form className="space-y-3" onSubmit={form.handleSubmit(onSubmit)}>
               <FormField
                 control={form.control}
-                name="expiryMonth"
+                name="cardHolder"
                 render={({ field }) => (
                   <FormItem>
-                    <Label htmlFor="month">Expires</Label>
-
-                    <Select onValueChange={field.onChange} disabled={isPending}>
-                      <FormControl>
-                        <SelectTrigger id="month">
-                          <SelectValue placeholder="Month" />
-                        </SelectTrigger>
-                      </FormControl>
-
-                      <SelectContent>
-                        <SelectItem value="1">January</SelectItem>
-                        <SelectItem value="2">February</SelectItem>
-                        <SelectItem value="3">March</SelectItem>
-                        <SelectItem value="4">April</SelectItem>
-                        <SelectItem value="5">May</SelectItem>
-                        <SelectItem value="6">June</SelectItem>
-                        <SelectItem value="7">July</SelectItem>
-                        <SelectItem value="8">August</SelectItem>
-                        <SelectItem value="9">September</SelectItem>
-                        <SelectItem value="10">October</SelectItem>
-                        <SelectItem value="11">November</SelectItem>
-                        <SelectItem value="12">December</SelectItem>
-                      </SelectContent>
-                    </Select>
-
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="expiryYear"
-                render={({ field }) => (
-                  <FormItem>
-                    <Label htmlFor="year">Year</Label>
-
-                    <Select onValueChange={field.onChange} disabled={isPending}>
-                      <FormControl>
-                        <SelectTrigger id="year">
-                          <SelectValue placeholder="Year" />
-                        </SelectTrigger>
-                      </FormControl>
-
-                      <SelectContent>
-                        {Array.from({ length: 10 }, (_, i) => (
-                          <SelectItem
-                            key={i}
-                            value={`${new Date().getFullYear() + i}`}
-                          >
-                            {new Date().getFullYear() + i}
-                          </SelectItem>
-                        ))}
-                      </SelectContent>
-                    </Select>
-
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="cvc"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel htmlFor="cvc">CVC</FormLabel>
-
+                    <FormLabel htmlFor="name">Name on Card</FormLabel>
                     <Input
-                      id="cvc"
-                      placeholder="CVC"
+                      id="name"
+                      defaultValue={card?.cardHolder}
                       onChange={field.onChange}
                       disabled={isPending}
                     />
-
-                    <FormMessage />
                   </FormItem>
                 )}
               />
-            </div>
 
-            <FormError message={error} />
-            <FormSuccess message={success} />
+              <div className="grid grid-cols-2 gap-4 pb-2">
+                <FormField
+                  control={form.control}
+                  name="expiryMonth"
+                  render={({ field }) => (
+                    <FormItem>
+                      <Label htmlFor="month">Expires</Label>
 
-            <div className="pt-1">
-              <Button disabled={isPending} className="w-full" type="submit">
-                {isPending && (
-                  <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                      <Select
+                        onValueChange={field.onChange}
+                        disabled={isPending}
+                        defaultValue={card?.expiryMonth}
+                      >
+                        <FormControl>
+                          <SelectTrigger id="month">
+                            <SelectValue defaultValue={card?.expiryMonth} />
+                          </SelectTrigger>
+                        </FormControl>
+
+                        <SelectContent>
+                          <SelectItem value="1">January</SelectItem>
+                          <SelectItem value="2">February</SelectItem>
+                          <SelectItem value="3">March</SelectItem>
+                          <SelectItem value="4">April</SelectItem>
+                          <SelectItem value="5">May</SelectItem>
+                          <SelectItem value="6">June</SelectItem>
+                          <SelectItem value="7">July</SelectItem>
+                          <SelectItem value="8">August</SelectItem>
+                          <SelectItem value="9">September</SelectItem>
+                          <SelectItem value="10">October</SelectItem>
+                          <SelectItem value="11">November</SelectItem>
+                          <SelectItem value="12">December</SelectItem>
+                        </SelectContent>
+                      </Select>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="expiryYear"
+                  render={({ field }) => (
+                    <FormItem>
+                      <Label htmlFor="year">Year</Label>
+
+                      <Select
+                        onValueChange={field.onChange}
+                        disabled={isPending}
+                        defaultValue={card?.expiryYear}
+                      >
+                        <FormControl>
+                          <SelectTrigger id="year">
+                            <SelectValue defaultValue={card?.expiryYear} />
+                          </SelectTrigger>
+                        </FormControl>
+
+                        <SelectContent>
+                          {Array.from({ length: 10 }, (_, i) => (
+                            <SelectItem
+                              key={i}
+                              value={`${new Date().getFullYear() + i}`}
+                            >
+                              {new Date().getFullYear() + i}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <FormField
+                control={form.control}
+                name="defaultCard"
+                render={({ field }) => (
+                  <FormItem className="flex items-center gap-2 pb-1">
+                    <FormLabel className="sr-only" htmlFor="default-payment">
+                      Default Payment
+                    </FormLabel>
+                    <FormControl>
+                      <Checkbox
+                        className="mr-0"
+                        id="defaultCard"
+                        onCheckedChange={field.onChange}
+                        disabled={isPending}
+                        checked={field.value}
+                        defaultChecked={card?.default}
+                      />
+                    </FormControl>
+
+                    <label
+                      htmlFor="terms"
+                      className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    >
+                      Set as default payment method
+                    </label>
+                  </FormItem>
                 )}
-                Add Payment Method
-              </Button>
-            </div>
-          </form>
-        </Form>
+              />
+
+              <FormError message={error} />
+              <FormSuccess message={success} />
+
+              <div className="pt-1">
+                <Button disabled={isPending} className="w-full" type="submit">
+                  {isPending && (
+                    <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                  )}
+                  Update Payment Method
+                </Button>
+              </div>
+            </form>
+          </Form>
+        )}
+
+        {!card && <Spinner color="primary" className="my-4" />}
       </CardContent>
     </Card>
   );
