666332a8d4f8cbcb732241c8766f722348b5b5d9
Julian <julianthant@gmail.com>
2024-04-05 17:16:18 -0700

    Refactor UserComponent in UserComponent.tsx

--- src/components/utils/UserComponent.tsx
+++ src/components/utils/UserComponent.tsx
@@ -1,20 +1,41 @@
 'use client';
 
 import { Skeleton } from '@nextui-org/react';
-import { AvatarFallback } from '../utils/Avatar';
 import { useCurrentUser } from '@/hooks/useCurrentUser';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { AvatarIcon } from '@nextui-org/react';
+import { cn } from '@/lib/utils';
 
-export const UserComponent = () => {
+type UserComponentProps = {
+  page: 'Navbar' | 'Profile';
+};
+
+export const UserComponent = ({ page }: UserComponentProps) => {
   const user = useCurrentUser();
 
   return (
     <div>
       {!!user ? (
-        <div className="py-2 flex items-center space-x-2">
-          <AvatarFallback userImage={user?.image} />
-          <div className="flex flex-col lg:w-[190px] w-[150px]">
+        <div
+          className={cn(
+            'py-2 flex items-center space-x-2',
+            page === 'Navbar' && 'w-[200px]'
+          )}
+        >
+          <Avatar className="w-9 h-9">
+            <AvatarImage src={user?.image as string} />
+            <AvatarFallback>
+              <AvatarIcon />
+            </AvatarFallback>
+          </Avatar>
+          <div
+            className={cn(
+              'flex flex-col lg:w-[190px] w-[150px]',
+              page === 'Navbar' && 'lg:w-full w-[160]'
+            )}
+          >
             <div className="truncate text-default-600 text-sm">
-              {user?.name || 'Guest'}
+              {user?.name}
             </div>
             <div className="truncate text-default-500 text-xs">
               {user?.email}
@@ -22,13 +43,13 @@ export const UserComponent = () => {
           </div>
         </div>
       ) : (
-        <div className="pb-2 p-3 w-[200px] flex items-center space-x-2">
+        <div className="py-2 w-[200px] flex items-center space-x-2">
           <div>
-            <Skeleton className="flex rounded-full w-8 h-8" />
+            <Skeleton className="flex rounded-full w-9 h-9" />
           </div>
           <div className="w-full flex flex-col gap-2">
-            <Skeleton className="h-3 w-3/5 rounded-lg" />
             <Skeleton className="h-3 w-4/5 rounded-lg" />
+            <Skeleton className="h-3 rounded-lg" />
           </div>
         </div>
       )}
