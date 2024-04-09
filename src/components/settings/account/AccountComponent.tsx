a1437a47a25369c4264770a6f6f21086369891ac
Julian <julianthant@gmail.com>
2024-04-09 02:06:42 -0700

    Refactor AccountComponent layout and remove unnecessary code

--- src/components/settings/account/AccountComponent.tsx
+++ src/components/settings/account/AccountComponent.tsx
@@ -20,27 +20,6 @@ export const AccountComponent = () => {
         <div className="space-y-2">
           <div className="flex">
             <Skeleton className="pr-4 rounded-lg h-6" isLoaded={!!user}>
-              <h3 className="font-semibold">Name</h3>
-            </Skeleton>
-
-            <Link
-              href={'/settings/edit/new-name'}
-              className="font-semibold ml-auto text-green-600"
-            >
-              Edit
-            </Link>
-          </div>
-
-          <Skeleton className="w-1/3 rounded-lg h-6" isLoaded={!!user}>
-            <p>{user?.name}</p>
-          </Skeleton>
-        </div>
-
-        <Divider />
-
-        <div className="space-y-2">
-          <div className="flex">
-            <Skeleton className="pr-4 rounded-lg h-6" isLoaded={!!user}>
               <h3 className="font-semibold">Email</h3>
             </Skeleton>
 
@@ -77,48 +56,6 @@ export const AccountComponent = () => {
 
           <Skeleton className="w-1/3 rounded-lg h-6" isLoaded={!!user}>
             <p>{'********' || 'N/A'}</p>
-          </Skeleton>
-        </div>
-
-        <Divider />
-
-        <div className="space-y-2">
-          <div className="flex">
-            <Skeleton className="pr-4 rounded-lg h-6" isLoaded={!!user}>
-              <h3 className="font-semibold">Country</h3>
-            </Skeleton>
-
-            <Link
-              href={'/settings/edit/change-country'}
-              className="font-semibold ml-auto text-green-600"
-            >
-              Edit
-            </Link>
-          </div>
-
-          <Skeleton className="w-1/3 rounded-lg h-6" isLoaded={!!user}>
-            <p>{user?.country || 'N/A'}</p>
-          </Skeleton>
-        </div>
-
-        <Divider />
-
-        <div className="space-y-2">
-          <div className="flex">
-            <Skeleton className="pr-4 rounded-lg h-6" isLoaded={!!user}>
-              <h3 className="font-semibold">Mobile Number</h3>
-            </Skeleton>
-
-            <Link
-              href={'/settings/edit/phone-number'}
-              className="font-semibold ml-auto text-green-600"
-            >
-              Edit
-            </Link>
-          </div>
-
-          <Skeleton className="w-1/3 rounded-lg h-6" isLoaded={!!user}>
-            <p>{user?.phoneNumber || 'N/A'}</p>
           </Skeleton>
         </div>
 
