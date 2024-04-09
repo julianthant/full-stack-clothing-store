883ed7f2d14c5b97f03926ff31b3663d701eb745
Julian <julianthant@gmail.com>
2024-04-09 01:54:16 -0700

    Add new layout components for address, payment, profile, and security settings

--- src/app/(main)/(dashboard)/settings/edit/layout.tsx
+++ src/app/(main)/(dashboard)/settings/edit/layout.tsx
@@ -10,13 +10,6 @@ const Layout: FC<LayoutProps> = ({ children }) => {
     <div className="mx-auto w-fit min-h-[300px]">
       <div className="grid space-y-3 border p-10 rounded-3xl mt-20">
         <div className="flex items-center justify-center">{children}</div>
-
-        <Link
-          className="text-center text-sm font-semibold mx-auto hover:underline underline-offset-4"
-          href="/settings?menu=Account"
-        >
-          Back to Settings
-        </Link>
       </div>
     </div>
   );
