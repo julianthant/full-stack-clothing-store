9ecff4b0a4bfddc4ef833de9c458aae32a86705b
Julian <julianthant@gmail.com>
2024-04-09 02:06:45 -0700

    Refactor ProfileComponent layout and remove unnecessary code

--- src/components/settings/account/ProfileComponent.tsx
+++ src/components/settings/account/ProfileComponent.tsx
@@ -5,7 +5,6 @@ import { Divider } from '@nextui-org/react';
 import { Skeleton } from '@nextui-org/react';
 
 import { useCurrentUser } from '@/hooks/useCurrentUser';
-import { sendTwoFactorActivationCode } from '@/actions/authentication/send-code';
 
 export const ProfileComponent = () => {
   const router = useRouter();
@@ -13,7 +12,6 @@ export const ProfileComponent = () => {
 
   return (
     <div className="space-y-8">
-      <h1 className="text-lg font-semibold md:text-2xl">Account Settings</h1>
       <div className="w-full rounded-lg border border-dashed shadow-sm p-8">
         <div className="space-y-4">
           <div className="space-y-2">
@@ -32,50 +30,6 @@ export const ProfileComponent = () => {
 
             <Skeleton className="w-1/3 rounded-lg h-6" isLoaded={!!user}>
               <p>{user?.name}</p>
-            </Skeleton>
-          </div>
-
-          <Divider />
-
-          <div className="space-y-2">
-            <div className="flex">
-              <Skeleton className="pr-4 rounded-lg h-6" isLoaded={!!user}>
-                <h3 className="font-semibold">Email</h3>
-              </Skeleton>
-
-              {!user?.isOAuth && (
-                <Link
-                  href={'/settings/edit/new-email'}
-                  className="font-semibold ml-auto text-green-600"
-                >
-                  Edit
-                </Link>
-              )}
-            </div>
-
-            <Skeleton className="w-1/3 rounded-lg h-6" isLoaded={!!user}>
-              <p>{user?.email}</p>
-            </Skeleton>
-          </div>
-
-          <Divider />
-
-          <div className="space-y-2">
-            <div className="flex">
-              <Skeleton className="pr-4 rounded-lg h-6" isLoaded={!!user}>
-                <h3 className="font-semibold">Password</h3>
-              </Skeleton>
-
-              <Link
-                href={'/settings/edit/reset-password'}
-                className="font-semibold ml-auto text-green-600"
-              >
-                Edit
-              </Link>
-            </div>
-
-            <Skeleton className="w-1/3 rounded-lg h-6" isLoaded={!!user}>
-              <p>{'********' || 'N/A'}</p>
             </Skeleton>
           </div>
 
@@ -118,45 +72,6 @@ export const ProfileComponent = () => {
 
             <Skeleton className="w-1/3 rounded-lg h-6" isLoaded={!!user}>
               <p>{user?.phoneNumber || 'N/A'}</p>
-            </Skeleton>
-          </div>
-
-          <Divider />
-
-          <div className="space-y-2">
-            <div className="flex">
-              <Skeleton className="pr-4 rounded-lg h-6" isLoaded={!!user}>
-                <h3 className="font-semibold">Two Factor Authentication</h3>
-              </Skeleton>
-
-              <form
-                className="ml-auto"
-                onSubmit={async (event) => {
-                  event.preventDefault();
-
-                  if (!user?.is2FAEnabled) {
-                    await sendTwoFactorActivationCode(user?.email as string);
-                    await router.push(
-                      '/settings/edit/two-factor-authentication?type=activate'
-                    );
-                  } else if (user?.is2FAEnabled) {
-                    await sendTwoFactorActivationCode(user?.email as string);
-                    await router.push(
-                      '/settings/edit/two-factor-authentication?type=deactivate'
-                    );
-                  }
-                }}
-              >
-                <button
-                  className="font-semibold ml-auto text-green-600"
-                  type="submit"
-                >
-                  {user?.is2FAEnabled ? 'Turn off' : 'Turn on'}
-                </button>
-              </form>
-            </div>
-            <Skeleton className="w-1/3 rounded-lg h-6" isLoaded={!!user}>
-              <p>{user?.is2FAEnabled ? 'Enabled' : 'Disabled'}</p>
             </Skeleton>
           </div>
         </div>
