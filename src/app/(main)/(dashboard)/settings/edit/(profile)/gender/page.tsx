5ab14af56716457073be3591bedde1cb0761180c
Julian <julianthant@gmail.com>
2024-04-10 22:25:01 -0700

    Add gender page component

---
+++ src/app/(main)/(dashboard)/settings/edit/(profile)/gender/page.tsx
@@ -0,0 +1,10 @@
+import { GenderForm } from '@/components/settings/account/forms/GenderForm';
+import { FC } from 'react';
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return <GenderForm />;
+};
+
+export default page;
