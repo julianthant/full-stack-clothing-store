092cd2965e05b53892dad2272042ddea68d2a836
Julian <julianthant@gmail.com>
2024-04-10 22:24:58 -0700

    Add date of birth page component

---
+++ src/app/(main)/(dashboard)/settings/edit/(profile)/date-of-birth/page.tsx
@@ -0,0 +1,10 @@
+import { DateOfBirthForm } from '@/components/settings/account/forms/DateOfBirthForm';
+import { FC } from 'react';
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return <DateOfBirthForm />;
+};
+
+export default page;
