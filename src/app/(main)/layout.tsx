c645a06a7a6545eb163199defb18ec3296202a77
Julian <julianthant@gmail.com>
2024-04-10 22:24:54 -0700

    Add ToastContainer component to main layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,6 +3,7 @@ import Navbar from '@/components/main/Navbar';
 import { SessionProvider } from 'next-auth/react';
 import { BreadCrumbs } from '@/components/ui/bread-crumbs';
 import Footer from '@/components/main/Footer';
+import { ToastContainer } from 'react-toastify';
 
 interface LayoutProps {
   children: ReactNode;
@@ -12,6 +13,19 @@ const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <SessionProvider>
       <Navbar />
+
+      <ToastContainer
+        pauseOnFocusLoss={false}
+        pauseOnHover={false}
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        draggable={false}
+        theme="dark"
+      />
 
       <div className="container py-5">
         <BreadCrumbs />
