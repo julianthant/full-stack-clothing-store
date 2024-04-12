338ec17d3e215964f746e3094c1010956aebe9ae
Julian <julianthant@gmail.com>
2024-04-11 23:14:46 -0700

    Refactor ProfileComponent to use EditUserComponent for user profile settings

--- src/components/settings/account/ProfileComponent.tsx
+++ src/components/settings/account/ProfileComponent.tsx
@@ -1,10 +1,8 @@
-import Link from 'next/link';
-
-import { cn } from '@/lib/utils';
 import { format } from 'date-fns';
 import { Divider } from '@nextui-org/react';
-import { Skeleton } from '@nextui-org/react';
 import { useCurrentUser } from '@/hooks/useCurrentUser';
+
+import EditUserComponent from '@/components/utils/EditUserComponent';
 
 export const ProfileComponent = () => {
   const user = useCurrentUser();
@@ -13,127 +11,51 @@ export const ProfileComponent = () => {
     <div className="space-y-8">
       <div className="w-full rounded-lg border border-dashed shadow-sm p-8">
         <div className="space-y-4">
-          <div className="space-y-2">
-            <div className="flex">
-              <Skeleton className="pr-4 rounded-lg h-6" isLoaded={!!user}>
-                <h3 className="font-semibold">Name</h3>
-              </Skeleton>
-
-              <Link
-                href={'/settings/edit/new-name'}
-                className="font-semibold ml-auto text-green-600"
-              >
-                Edit
-              </Link>
-            </div>
-
-            <Skeleton
-              className={cn('rounded-lg h-6', !!user ? 'w-full' : 'w-1/3')}
-              isLoaded={!!user}
-            >
-              <p>{user?.name}</p>
-            </Skeleton>
-          </div>
+          <EditUserComponent
+            Title="Name"
+            Name={user?.name || 'N/A'}
+            FormLink={'/settings/edit/change-name'}
+          />
 
           <Divider />
 
-          <div className="space-y-2">
-            <div className="flex">
-              <Skeleton className="pr-4 rounded-lg h-6" isLoaded={!!user}>
-                <h3 className="font-semibold">Country</h3>
-              </Skeleton>
-
-              <Link
-                href={'/settings/edit/change-country'}
-                className="font-semibold ml-auto text-green-600"
-              >
-                Edit
-              </Link>
-            </div>
-
-            <Skeleton
-              className={cn('rounded-lg h-6', !!user ? 'w-full' : 'w-1/3')}
-              isLoaded={!!user}
-            >
-              <p>{user?.country || 'N/A'}</p>
-            </Skeleton>
-          </div>
+          <EditUserComponent
+            Title="Country"
+            Name={user?.country || 'N/A'}
+            FormLink={'/settings/edit/change-country'}
+          />
 
           <Divider />
 
-          <div className="space-y-2">
-            <div className="flex">
-              <Skeleton className="pr-4 rounded-lg h-6" isLoaded={!!user}>
-                <h3 className="font-semibold">Gender</h3>
-              </Skeleton>
-
-              <Link
-                href={'/settings/edit/gender'}
-                className="font-semibold ml-auto text-green-600"
-              >
-                Edit
-              </Link>
-            </div>
-
-            <Skeleton
-              className="w-1/3 rounded-lg h-6 capitalize"
-              isLoaded={!!user}
-            >
-              <p>{user?.gender || 'N/A'}</p>
-            </Skeleton>
-          </div>
+          <EditUserComponent
+            Title="Date Of Birth"
+            Name={
+              (user?.dateOfBirth &&
+                format(user?.dateOfBirth as Date, 'dd MMMM, yyy')) ||
+              'N/A'
+            }
+            FormLink={'/settings/edit/date-of-birth'}
+          />
 
           <Divider />
 
-          <div className="space-y-2">
-            <div className="flex">
-              <Skeleton className="pr-4 rounded-lg h-6" isLoaded={!!user}>
-                <h3 className="font-semibold">Date Of Birth</h3>
-              </Skeleton>
-
-              <Link
-                href={'/settings/edit/date-of-birth'}
-                className="font-semibold ml-auto text-green-600"
-              >
-                Edit
-              </Link>
-            </div>
-
-            <Skeleton
-              className={cn('rounded-lg h-6', !!user ? 'w-full' : 'w-1/3')}
-              isLoaded={!!user}
-            >
-              <p>
-                {(user?.dateOfBirth &&
-                  format(user?.dateOfBirth as Date, 'dd MMMM, yyy')) ||
-                  'N/A'}
-              </p>
-            </Skeleton>
-          </div>
+          <EditUserComponent
+            Title="Gender"
+            Name={
+              (user?.gender &&
+                user?.gender.charAt(0).toUpperCase() + user?.gender.slice(1)) ||
+              'N/A'
+            }
+            FormLink={'/settings/edit/gender'}
+          />
 
           <Divider />
 
-          <div className="space-y-2">
-            <div className="flex">
-              <Skeleton className="pr-4 rounded-lg h-6" isLoaded={!!user}>
-                <h3 className="font-semibold">Primary Mobile Number</h3>
-              </Skeleton>
-
-              <Link
-                href={'/settings/edit/phone-number'}
-                className="font-semibold ml-auto text-green-600"
-              >
-                Edit
-              </Link>
-            </div>
-
-            <Skeleton
-              className={cn('rounded-lg h-6', !!user ? 'w-full' : 'w-1/3')}
-              isLoaded={!!user}
-            >
-              <p>{user?.phoneNumber || 'N/A'}</p>
-            </Skeleton>
-          </div>
+          <EditUserComponent
+            Title="Primary Mobile Number"
+            Name={user?.phoneNumber || 'N/A'}
+            FormLink={'/settings/edit/phone-number'}
+          />
         </div>
       </div>
     </div>
