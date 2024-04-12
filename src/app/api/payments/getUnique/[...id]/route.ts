1a0afe0a7110a92295ad5e925c08218cdb94d53f
Julian <julianthant@gmail.com>
2024-04-11 23:14:31 -0700

    Refactor GET function in route.ts to remove console.log statement

--- src/app/api/payments/getUnique/[...id]/route.ts
+++ src/app/api/payments/getUnique/[...id]/route.ts
@@ -14,8 +14,6 @@ export async function GET(req: NextRequest) {
       where: { id },
     });
 
-    console.log(paymentMethod);
-
     return NextResponse.json(paymentMethod);
   } catch (error) {
     return;
