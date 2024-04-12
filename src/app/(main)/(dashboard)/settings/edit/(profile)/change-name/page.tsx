6c4859c5af7fbf6faf5749664a5e58230f7b8932
Julian <julianthant@gmail.com>
2024-04-12 15:49:08 -0700

    Add new page for changing profile name in settings

---
+++ src/app/(main)/(dashboard)/settings/edit/(profile)/change-name/page.tsx
@@ -0,0 +1,10 @@
+import { NameEditForm } from '@/components/settings/account/forms/NameEditForm';
+import { FC } from 'react';
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return <NameEditForm />;
+};
+
+export default page;
