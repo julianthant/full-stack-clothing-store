e4b30029d28ce9d0abd4fff6997e89df113ed99f
Julian <julianthant@gmail.com>
2024-04-13 02:57:50 -0700

    Add NewArrivalsComponent and TopSellingComponent to home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,11 +1,23 @@
 import { BrandsComponent } from '@/components/home/BrandsComponent';
 import { HeadingComponent } from '@/components/home/HeadingComponent';
+import { NewArrivalsComponent } from '@/components/home/NewArrivalsComponent';
+import { TopSellingComponent } from '@/components/home/TopSellingComponent';
+import { Divider } from '@nextui-org/react';
 
 export default function Home() {
   return (
-    <section>
-      <HeadingComponent />
-      <BrandsComponent />
+    <section className="grid gap-16">
+      <div>
+        <HeadingComponent />
+        <BrandsComponent />
+      </div>
+      <NewArrivalsComponent />
+
+      <div className="container">
+        <Divider />
+      </div>
+
+      <TopSellingComponent />
     </section>
   );
 }
