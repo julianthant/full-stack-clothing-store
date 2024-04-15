ba4095af611b9311693d1ac0178dd0fc7f567110
Julian <julianthant@gmail.com>
2024-04-15 01:17:44 -0700

    Refactor BrandsComponent to update logo sizes and spacing

--- src/components/home/BrandsComponent.tsx
+++ src/components/home/BrandsComponent.tsx
@@ -2,14 +2,14 @@ import { Icons } from '../utils/Icons';
 
 export const BrandsComponent = ({}) => {
   return (
-    <div className="bg-black h-24 flex items-center">
-      <div className="container">
-        <div className="flex items-center justify-between">
-          <Icons.versaceLogo />
-          <Icons.zaraLogo />
-          <Icons.gucciLogo />
-          <Icons.pradaLogo />
-          <Icons.CKLogo />
+    <div className="bg-black lg:h-24 h-36 flex items-center">
+      <div className="container max-lg:px-0 max-sm:px-4 max-lg:max-w-[520px]">
+        <div className="flex items-center lg:justify-between max-lg:flex-wrap justify-center max-lg:gap-x-10 max-lg:gap-y-6 max-sm:gap-x-5 gap-y-4 ">
+          <Icons.versaceLogo className="max-sm:w-[126px]" />
+          <Icons.zaraLogo className="max-md:w-20 max-sm:w-16" />
+          <Icons.gucciLogo className="max-sm:w-28" />
+          <Icons.pradaLogo className="max-sm:w-32 w-[185px]" />
+          <Icons.CKLogo className="max-sm:w-32" />
         </div>
       </div>
     </div>
