7cd6b35a9931c757fc22a6be16fcd4d284fe72b9
Julian <julianthant@gmail.com>
2024-04-15 05:57:57 -0700

    Update NewsletterComponent styles and responsiveness

--- src/components/main/NewsletterComponent.tsx
+++ src/components/main/NewsletterComponent.tsx
@@ -7,20 +7,21 @@ import { Icons } from '../utils/Icons';
 
 export const NewsletterComponent = ({}) => {
   return (
-    <div className="absolute container top-[-256px] left-0">
-      <div className="bg-black rounded-[23px] flex items-center justify-between h-48 px-20">
-        <div className="w-[600px]">
-          <p className="text-white text-5xl font-black">
+    <div className="absolute container lg:top-[-256px] sm:top-[-282px] top-[-296px] left-0">
+      <div className="bg-black rounded-[23px] flex items-center justify-between lg:h-48 lg:px-20 max-lg:flex-col max-lg:py-6 max-lg:gap-y-4 max-sm:px-4">
+        <div className="lg:w-[600px] lg:px-0 lg:text-left md:px-8 sm:text-center ">
+          <p className="text-white lg:text-5xl sm:text-4xl text-3xl font-black">
             STAY UPTO DATE ABOUT OUR LATEST OFFERS
           </p>
         </div>
 
-        <div className="space-y-3">
+        <div className="space-y-3 max-lg:w-full px-0 max-lg:px-24 max-md:px-8 max-sm:px-0">
           <Input
             type="email"
             placeholder="Enter your email address"
             classNames={{
-              inputWrapper: 'rounded-full w-[350px] h-[48px] bg-white',
+              inputWrapper:
+                'rounded-full lg:w-[350px] w-full lg:h-[48px] h-[44px] bg-white',
             }}
             startContent={
               <Icons.mailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
@@ -28,7 +29,7 @@ export const NewsletterComponent = ({}) => {
           />
           <Button
             variant={'outline'}
-            className="bg-white text-black rounded-full w-[350px] h-12"
+            className="bg-white text-black rounded-full lg:w-[350px] w-full lg:h-12 h-11"
           >
             <Link href={'/'}>Subscribe to Newsletter</Link>
           </Button>
