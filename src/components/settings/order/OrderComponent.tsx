f856595ca3edc3eaf7790e6d0e14255983ad0c04
Julian <julianthant@gmail.com>
2024-04-16 02:02:07 -0700

    Refactor font styling in OrderComponent.tsx

--- src/components/settings/order/OrderComponent.tsx
+++ src/components/settings/order/OrderComponent.tsx
@@ -1,13 +1,9 @@
 'use client';
 
-import { gabarito } from '../../utils/Fonts';
-
 export const OrderComponent = () => {
   return (
     <div>
-      <h1
-        className={`font-black text-inherit text-4xl ${gabarito.className} drop-shadow-lg`}
-      >
+      <h1 className="font-black text-inherit text-4xl drop-shadow-lg">
         YOUR ORDERS
       </h1>
     </div>
