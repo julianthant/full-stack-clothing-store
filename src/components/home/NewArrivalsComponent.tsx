f2e2c8d58394dc418fc9012fb76f5dcf10830f35
Julian <julianthant@gmail.com>
2024-04-16 02:01:26 -0700

    Update images paths in NewArrivalsComponent

--- src/components/home/NewArrivalsComponent.tsx
+++ src/components/home/NewArrivalsComponent.tsx
@@ -1,8 +1,3 @@
-import Shirt1 from '../images/Shirt-1.png';
-import Shirt2 from '../images/Shirt-2.png';
-import Shirt3 from '../images/Shirt-3.png';
-import Pants1 from '../images/Pants-1.png';
-
 import { ClothesComponent } from '../utils/ClothesComponent';
 import { Button } from '../ui/button';
 import Link from 'next/link';
@@ -13,37 +8,38 @@ export const NewArrivalsComponent = ({}) => {
       id: 1,
       name: 'T-shirt with Tape Details',
       price: '20',
-      image: Shirt3,
+      image: '/images/Shirt-3.png',
       rating: '4.5',
     },
     {
       id: 2,
       name: 'Skinny Fit Jeans',
       price: '25',
-      image: Pants1,
+      image: '/images/Pants-1.png',
       rating: '4',
     },
     {
       id: 3,
       name: 'Checkered Shirt',
       price: '30',
-      image: Shirt2,
+      image: '/images/Shirt-2.png',
       rating: '3.5',
     },
     {
       id: 4,
       name: 'Sleeve Striped T-shirt',
       price: '35',
-      image: Shirt1,
+      image: '/images/Shirt-1.png',
       rating: '2',
     },
   ];
+
   return (
     <div className="container lg:space-y-14 space-y-7 flex flex-col lg:mt-5">
-      <h1 className="lg:text-5xl text-3xl font-black text-center">
+      <h1 className="lg:text-5xl text-3xl font-bold bold-integral text-center">
         NEW ARRIVALS
       </h1>
-      <div className="flex gap-x-5 max-lg:max-w-max overflow-x-scroll overflow-visible">
+      <div className="flex gap-x-5 max-lg:max-w-max max-lg:overflow-x-scroll scroll-smooth">
         {clothes.map((item) => (
           <ClothesComponent
             key={item.id}
