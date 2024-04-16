e3732b8aad04b3de073ca35f46ce489d16a0bb9f
Julian <julianthant@gmail.com>
2024-04-16 02:23:25 -0700

    Update font styling and text sizes in NavbarPages.tsx

--- src/components/main/NavbarPages.tsx
+++ src/components/main/NavbarPages.tsx
@@ -13,71 +13,70 @@ import {
 
 export const NavbarPages = ({}) => {
   return (
-    <>
-      <NavbarContent className="hidden lg:flex gap-4 px-4" justify="center">
-        <Dropdown className="w-[100px]">
-          <NavbarItem>
-            <DropdownTrigger>
-              <Button
-                disableRipple
-                className="p-0 bg-transparent data-[hover=true]:bg-transparent min-w-min text-md font-medium"
-                endContent={<Icons.chevronDown fill="currentColor" size={16} />}
-                radius="sm"
-                variant="light"
-              >
-                Shop
-              </Button>
-            </DropdownTrigger>
-          </NavbarItem>
-
-          <DropdownMenu
-            aria-label="ACME features"
-            itemClasses={{
-              base: 'gap-4',
-            }}
-          >
-            <DropdownItem href="/shop/men">Men&apos;s Wear</DropdownItem>
-            <DropdownItem href="/shop/women">Women&apos;s Wear</DropdownItem>
-            <DropdownItem href="/shop/children">
-              Children&apos;s Wear
-            </DropdownItem>
-            <DropdownItem href="/shop/casual">Casual Outfits</DropdownItem>
-            <DropdownItem href="/shop/formal">Formal Clothing</DropdownItem>
-            <DropdownItem href="/shop/party">Party Outfits</DropdownItem>
-            <DropdownItem href="/shop/sports">Sports Wear</DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-
+    <NavbarContent className="hidden lg:flex gap-4 pl-8 pr-4" justify="center">
+      <Dropdown className="w-[100px]">
         <NavbarItem>
-          <Link
-            color="foreground"
-            className="hover:font-bold transition-all"
-            href="/shop"
-          >
-            On Sale
-          </Link>
+          <DropdownTrigger>
+            <Button
+              disableRipple
+              className="p-0 bg-transparent data-[hover=true]:bg-transparent data-[hover=true]:font-medium min-w-min text-base font-light"
+              endContent={<Icons.chevronDown fill="currentColor" size={16} />}
+              radius="sm"
+              variant="light"
+            >
+              Shop
+            </Button>
+          </DropdownTrigger>
         </NavbarItem>
 
-        <NavbarItem>
-          <Link
-            color="foreground"
-            href="/shop/new-arrivals"
-            className="hover:font-bold transition-all"
-          >
-            New Arrivals
-          </Link>
-        </NavbarItem>
+        <DropdownMenu
+          aria-label="ACME features"
+          itemClasses={{
+            base: 'gap-4',
+          }}
+          className="text-base font-light"
+        >
+          <DropdownItem href="/shop/men">Men&apos;s Wear</DropdownItem>
+          <DropdownItem href="/shop/women">Women&apos;s Wear</DropdownItem>
+          <DropdownItem href="/shop/children">
+            Children&apos;s Wear
+          </DropdownItem>
+          <DropdownItem href="/shop/casual">Casual Outfits</DropdownItem>
+          <DropdownItem href="/shop/formal">Formal Clothing</DropdownItem>
+          <DropdownItem href="/shop/party">Party Outfits</DropdownItem>
+          <DropdownItem href="/shop/sports">Sports Wear</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
 
-        <NavbarItem>
-          <Link
-            color="foreground"
-            href="/shop/brands"
-            className="hover:font-bold transition-all"
-          >
-            Brands
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-    </>
+      <NavbarItem>
+        <Link
+          color="foreground"
+          className="hover:font-medium transition-all text-base font-light"
+          href="/shop"
+        >
+          On Sale
+        </Link>
+      </NavbarItem>
+
+      <NavbarItem>
+        <Link
+          color="foreground"
+          href="/shop/new-arrivals"
+          className="hover:font-medium transition-all text-base font-light"
+        >
+          New Arrivals
+        </Link>
+      </NavbarItem>
+
+      <NavbarItem>
+        <Link
+          color="foreground"
+          href="/shop/brands"
+          className="hover:font-medium transition-all text-base font-light"
+        >
+          Brands
+        </Link>
+      </NavbarItem>
+    </NavbarContent>
   );
 };
