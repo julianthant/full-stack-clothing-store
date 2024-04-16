4c96d31a6811c60c431d5a8d243fe89a6ccad683
Julian <julianthant@gmail.com>
2024-04-16 02:02:02 -0700

    Update font styling in CartComponent.tsx

--- src/components/settings/cart/CartComponent.tsx
+++ src/components/settings/cart/CartComponent.tsx
@@ -1,13 +1,9 @@
 'use client';
 
-import { gabarito } from '../../utils/Fonts';
-
 export const CartComponent = () => {
   return (
     <div>
-      <h1
-        className={`font-black text-inherit text-4xl ${gabarito.className} drop-shadow-lg`}
-      >
+      <h1 className="font-black text-inherit text-4xl drop-shadow-lg">
         YOUR CART
       </h1>
     </div>
