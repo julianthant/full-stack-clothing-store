d05e416927d2506e7414bf080d3c3740832f6507
Julian <julianthant@gmail.com>
2024-04-17 00:05:24 -0700

    Update NewsletterComponent with form validation and submission logic

--- src/components/main/NewsletterComponent.tsx
+++ src/components/main/NewsletterComponent.tsx
@@ -1,33 +1,73 @@
 'use client';
 
+import * as z from 'zod';
 import Link from 'next/link';
-import { Button } from '../ui/button';
+
 import { Input } from '@nextui-org/react';
 import { Icons } from '../utils/Icons';
+import { Button } from '../ui/button';
 
-export const NewsletterComponent = ({}) => {
+import { useForm } from 'react-hook-form';
+import { EmailSchema } from '@/schemas';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useTransition } from 'react';
+
+import { Form, FormField, FormItem, FormLabel } from '@/components/ui/form';
+
+export const NewsletterComponent = () => {
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof EmailSchema>>({
+    resolver: zodResolver(EmailSchema),
+  });
+
+  const onSubmit = (values: z.infer<typeof EmailSchema>) => {
+    startTransition(() => {
+      console.log(values);
+    });
+  };
+
   return (
-    <div className="absolute container lg:top-[-256px] sm:top-[-282px] top-[-296px] left-0">
-      <div className="bg-black rounded-[23px] flex items-center justify-between lg:h-48 lg:px-20 max-lg:flex-col max-lg:py-6 max-lg:gap-y-4 max-sm:px-4">
-        <div className="lg:w-[650px] lg:px-0 lg:text-left md:px-8 sm:text-center ">
+    <div className="container relative z-10">
+      <div className="bg-black rounded-[23px] flex items-center justify-between lg:h-48 h-full lg:px-20 max-lg:flex-col max-lg:py-6 max-lg:gap-y-4 max-sm:px-4">
+        <div className="lg:w-[650px] w-full lg:px-0 lg:text-left md:px-8 sm:text-center ">
           <p className="text-white lg:text-5xl sm:text-4xl text-3xl font-bold bold-integral">
             STAY UPTO DATE ABOUT OUR LATEST OFFERS
           </p>
         </div>
 
         <div className="space-y-3 max-lg:w-full px-0 max-lg:px-24 max-md:px-8 max-sm:px-0">
-          <Input
-            type="email"
-            placeholder="Enter your email address"
-            classNames={{
-              inputWrapper:
-                'rounded-full lg:w-[350px] w-full lg:h-[48px] h-[44px] bg-white',
-            }}
-            startContent={
-              <Icons.mailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-            }
-          />
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="sr-only" htmlFor="email">
+                      Email
+                    </FormLabel>
+                    <Input
+                      type="email"
+                      placeholder="Enter your email address"
+                      classNames={{
+                        inputWrapper:
+                          'rounded-full lg:w-[350px] w-full lg:h-[48px] h-[44px] bg-white',
+                      }}
+                      startContent={
+                        <Icons.mailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                      }
+                      suppressHydrationWarning={true}
+                    />
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+
           <Button
+            type="submit"
+            asChild
             variant={'outline'}
             className="bg-white text-black rounded-full lg:w-[350px] w-full lg:h-12 h-11"
           >
