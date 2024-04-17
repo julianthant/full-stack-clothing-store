9c63ad8bc535fb78aa53fdb0183e651cdce6aa55
Julian <julianthant@gmail.com>
2024-04-17 00:05:21 -0700

    Update Footer component with new styles and content

--- src/components/main/Footer.tsx
+++ src/components/main/Footer.tsx
@@ -92,54 +92,59 @@ export const Footer = () => {
   ];
 
   return (
-    <div className="bg-foreground-100 pb-14 pt-28 lg:mt-40 mt-48">
-      <div className="container space-y-12 relative">
+    <div className="flex flex-col pt-16">
+      <div className="relative">
         <NewsletterComponent />
-        <nav className="lg:flex items-center justify-between grid w-full sm:grid-rows-[8rem]  grid-cols-2 gap-y-6">
-          <div className="lg:w-[261px] lg:space-y-5 space-y-3 font-light place-self-stretch col-span-2 h-min">
-            <Link
-              href="/"
-              className={`font-bold bold-integral text-inherit text-4xl drop-shadow-lg max-sm:text-3xl `}
-            >
-              CLOTHES.CO
-            </Link>
-            <p className="text-sm text-foreground-500 leading-7 font-light">
-              We have clothes that suits your style and which you&apos;re proud
-              to wear. From women to men.
+        <div className="bg-foreground-100 w-full absolute top-1/2 h-1/2"></div>
+      </div>
+
+      <div className="bg-foreground-100 pb-14 pt-12">
+        <div className="container space-y-12 relative">
+          <nav className="lg:flex items-center justify-between grid w-full sm:grid-rows-[8rem]  grid-cols-2 gap-y-6">
+            <div className="lg:w-[261px] lg:space-y-5 space-y-3 font-light place-self-stretch col-span-2 h-min">
+              <Link
+                href="/"
+                className={`font-bold bold-integral text-inherit text-4xl drop-shadow-lg max-sm:text-3xl`}
+              >
+                CLOTHES.CO
+              </Link>
+              <p className="text-sm text-foreground-500 leading-7 font-light">
+                We have clothes that suits your style and which you&apos;re
+                proud to wear. From women to men.
+              </p>
+              <div className="flex items-center gap-x-5 pt-1">
+                <Icons.facebook className="w-6 h-6" />
+                <Icons.gitHub className="w-6 h-6" />
+                <Icons.twitter className="w-6 h-6" />
+                <Icons.google className="w-6 h-6" />
+              </div>
+            </div>
+
+            {FooterNavs.map((nav, index) => (
+              <div key={index} className="lg:space-y-5 space-y-4">
+                <h3 className="font-medium text-base tracking-[0.15em]">
+                  {nav.title}
+                </h3>
+                <div className="lg:space-y-5 space-y-4 text-foreground-500">
+                  {nav.pages.map((page, index) => (
+                    <div key={index}>
+                      <Link href={page.link} className="text-base font-light">
+                        {page.title}
+                      </Link>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </nav>
+
+          <Divider />
+
+          <div className="flex items-center sm:justify-between justify-center">
+            <p className="text-sm text-foreground-500 leading-7 font-light ">
+              Clothes.co &copy; 2000-2024. All Rights Reserved
             </p>
-            <div className="flex items-center gap-x-5 pt-1">
-              <Icons.facebook className="w-6 h-6" />
-              <Icons.gitHub className="w-6 h-6" />
-              <Icons.twitter className="w-6 h-6" />
-              <Icons.google className="w-6 h-6" />
-            </div>
           </div>
-
-          {FooterNavs.map((nav, index) => (
-            <div key={index} className="lg:space-y-5 space-y-4">
-              <h3 className="font-medium text-base tracking-[0.15em]">
-                {nav.title}
-              </h3>
-              <ul className="lg:space-y-5 space-y-4 text-foreground-500">
-                {nav.pages.map((page, index) => (
-                  <li key={index}>
-                    <Link href={page.link} className="text-base font-light">
-                      {page.title}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </nav>
-
-        <Divider />
-
-        <div className="flex items-center sm:justify-between justify-center">
-          <p className="text-sm text-foreground-500 leading-7 font-light ">
-            Clothes.co &copy; 2000-2024. All Rights Reserved
-          </p>
-          <div></div>
         </div>
       </div>
     </div>
