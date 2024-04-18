ca2e87d2ca16e141647fe0bb1bfa9a7715cda060
Julian <julianthant@gmail.com>
2024-04-18 03:32:29 -0700

    Update ClothesComponent with hover image functionality

--- src/components/utils/ClothesComponent.tsx
+++ src/components/utils/ClothesComponent.tsx
@@ -1,5 +1,5 @@
+import { cn } from '@/lib/utils';
 import Image from 'next/image';
-import { Icons } from './Icons';
 import Link from 'next/link';
 
 type ClothesComponentProps = {
@@ -7,6 +7,7 @@ type ClothesComponentProps = {
   Price: string;
   ItemImage: string;
   Rating: string;
+  HoverImage?: string;
 };
 
 export const ClothesComponent = ({
@@ -14,45 +15,44 @@ export const ClothesComponent = ({
   Price,
   ItemImage,
   Rating,
+  HoverImage,
 }: ClothesComponentProps) => {
-  const rating = Rating[0];
-  const half = Rating.length > 1;
-
   return (
     <div className="flex flex-col lg:gap-5 gap-2">
-      <div className=" max-lg:w-[200px]">
+      <div className="max-lg:w-[200px] relative">
         <Image
           src={ItemImage}
           alt={Name}
           width={360}
           height={360}
-          className="hover:cursor-pointer"
+          className={cn('hover:cursor-pointer z-10 transition-opacity', {
+            'hover:opacity-0': HoverImage,
+          })}
           priority
           loading="eager"
+          quality={100}
         />
+
+        {HoverImage && (
+          <Image
+            src={HoverImage}
+            alt={Name}
+            width={360}
+            height={360}
+            className="hover:cursor-pointer absolute inset-0 opacity-0 transition-opacity hover:opacity-100 z-20"
+            priority
+            loading="eager"
+            quality={100}
+          />
+        )}
       </div>
 
       <div className="lg:space-y-2">
-        <Link
-          href={''}
-          className="2xl:text-2xl xl:text-xl lg:text-lg text-sm font-semibold"
-        >
+        <Link href={''} className="text-sm font-light">
           {Name}
         </Link>
 
-        <div className="flex items-center gap-2">
-          {Array.from({ length: parseInt(rating, 10) }, (_, i) => (
-            <Icons.star key={i} className="lg:w-max w-4" />
-          ))}
-          {half && <Icons.halfStar className="lg:w-max w-4" />}
-          <p className="text-sm">
-            {Rating}/<span className="text-sm text-foreground-400">5</span>
-          </p>
-        </div>
-
-        <p className="2xl:text-3xl xl:text-2xl lg:text-xl text-lg font-bold">
-          ${Price}
-        </p>
+        <p className="text-sm text-foreground-500 font-bold">{Price}</p>
       </div>
     </div>
   );
