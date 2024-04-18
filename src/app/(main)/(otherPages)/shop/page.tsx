734d7de1a42a3bebf62dc8427c3c7493ae4622b2
Julian <julianthant@gmail.com>
2024-04-18 03:32:01 -0700

    Add FilterClothes and ClothesGalleryComponent to shop page

--- src/app/(main)/(otherPages)/shop/page.tsx
+++ src/app/(main)/(otherPages)/shop/page.tsx
@@ -1,9 +1,16 @@
+import { ClothesGalleryComponent } from '@/components/shop/ClothesGalleryComponent';
+import { FilterClothes } from '@/components/shop/FilterClothes';
 import { FC } from 'react';
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
-  return <div>page</div>;
+  return (
+    <div className="flex gap-6">
+      <FilterClothes />
+      <ClothesGalleryComponent />
+    </div>
+  );
 };
 
 export default page;
