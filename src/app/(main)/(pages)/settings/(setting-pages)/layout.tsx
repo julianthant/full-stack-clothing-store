6a66b846efa420246ebf866ac16ca47c6da21d25
Julian <julianthant@gmail.com>
2024-04-23 19:46:12 -0700

    Refactor layout component in settings section

--- src/app/(main)/(pages)/settings/(setting-pages)/layout.tsx
+++ src/app/(main)/(pages)/settings/(setting-pages)/layout.tsx
@@ -2,6 +2,9 @@ import { FC, ReactNode } from 'react';
 import { Button } from '@/components/ui/button';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 
+import { UserComponent } from '@/components/utils/UserComponent';
+import { MenuLink } from '@/components/utils/MenuLink';
+
 import {
   Home,
   LineChart,
@@ -11,7 +14,6 @@ import {
   Users,
 } from 'lucide-react';
 import Link from 'next/link';
-import { MenuLink } from '@/components/utils/MenuLink';
 import { SubLink } from '@/components/utils/SubLink';
 
 interface LayoutProps {
@@ -49,65 +51,94 @@ const Layout: FC<LayoutProps> = ({ children }) => {
   ];
 
   return (
-    <div className="flex flex-col relative">
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button
-            variant="outline"
-            size="icon"
-            className="shrink-0 absolute top-2.5 left-4 lg:hidden"
-          >
-            <Menu className="h-5 w-5" />
-            <span className="sr-only">Toggle navigation menu</span>
-          </Button>
-        </SheetTrigger>
-        <SheetContent
-          side="left"
-          className="flex flex-col overflow-y-auto max-h-screen"
-        >
-          <nav className="grid text-lg font-medium">
-            <Link
-              href="/"
-              className="flex items-center gap-2 text-lg font-semibold pb-2"
-            >
-              CLOTHES.CO
-              <span className="sr-only">Clothes.CO</span>
-            </Link>
+    <div className="container">
+      <div className="grid border rounded-[20px] w-full md:grid-cols-[250px_1fr] lg:grid-cols-[280px_1fr] mb-16">
+        <div className="hidden border-r bg-muted/40 md:block rounded-l-[20px]">
+          <div className="flex h-full max-h-dvh flex-col gap-2">
+            <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+              <UserComponent page="Profile" />
+            </div>
 
-            <div className="grid gap-2">
-              {NavigationLinks.map((link) => (
-                <div className="grid gap-2" key={link.mainPage}>
-                  <Button
-                    asChild
-                    variant={'ghost'}
-                    className="flex items-center justify-start w-full"
+            <div className="flex-1">
+              <nav className="grid gap-1 items-start px-2 text-sm font-medium lg:px-4 pb-2">
+                {NavigationLinks.map((link) => (
+                  <MenuLink
+                    key={link.mainPage}
+                    title={link.mainPage}
+                    path={link.subPages[0]}
+                  />
+                ))}
+              </nav>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col">
+          <div className="flex flex-col relative">
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="shrink-0 absolute top-2.5 left-4 lg:hidden"
+                >
+                  <Menu className="h-5 w-5" />
+                  <span className="sr-only">Toggle navigation menu</span>
+                </Button>
+              </SheetTrigger>
+              <SheetContent
+                side="left"
+                className="flex flex-col overflow-y-auto max-h-screen"
+              >
+                <nav className="grid text-lg font-medium">
+                  <Link
+                    href="/"
+                    className="flex items-center gap-2 text-lg font-semibold pb-2"
                   >
-                    <MenuLink title={link.mainPage} path={link.subPages[0]} />
-                  </Button>
+                    CLOTHES.CO
+                    <span className="sr-only">Clothes.CO</span>
+                  </Link>
 
-                  <div className="grid place-items-start pl-4 gap-1">
-                    {link.subPages.map((subPage) => (
-                      <Button
-                        key={subPage}
-                        asChild
-                        variant="ghost"
-                        className="w-52 flex items-center justify-start"
-                      >
-                        <SubLink
-                          title={subPage}
-                          path={`${link.mainPage}/${subPage}`}
-                        />
-                      </Button>
+                  <div className="grid gap-2">
+                    {NavigationLinks.map((link) => (
+                      <div className="grid gap-2" key={link.mainPage}>
+                        <Button
+                          asChild
+                          variant={'ghost'}
+                          className="flex items-center justify-start w-full"
+                        >
+                          <MenuLink
+                            title={link.mainPage}
+                            path={link.subPages[0]}
+                          />
+                        </Button>
+
+                        <div className="grid place-items-start pl-4 gap-1">
+                          {link.subPages.map((subPage) => (
+                            <Button
+                              key={subPage}
+                              asChild
+                              variant="ghost"
+                              className="w-52 flex items-center justify-start"
+                            >
+                              <SubLink
+                                title={subPage}
+                                path={`${link.mainPage}/${subPage}`}
+                              />
+                            </Button>
+                          ))}
+                        </div>
+                      </div>
                     ))}
                   </div>
-                </div>
-              ))}
-            </div>
-          </nav>
-        </SheetContent>
-      </Sheet>
+                </nav>
+              </SheetContent>
+            </Sheet>
 
-      {children}
+            {children}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
