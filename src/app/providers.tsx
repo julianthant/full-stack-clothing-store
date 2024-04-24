ef21046a1bea59272722996ad862e072ca2b132b
Julian <julianthant@gmail.com>
2024-04-24 16:59:57 -0700

    Refactor QueryProvider in providers.tsx to use useState for queryClient initialization

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -7,18 +7,22 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return <NextUIProvider>{children}</NextUIProvider>;
 }
 
+import { useState } from 'react';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 export function QueryProvider({ children }: { children: React.ReactNode }) {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-        refetchOnMount: false,
-        staleTime: 1000 * 60 * 10,
-      },
-    },
-  });
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            refetchOnMount: false,
+            staleTime: 1000 * 60 * 10,
+          },
+        },
+      })
+  );
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
