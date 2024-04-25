47bf399e714cfe98040967f028dfa19bf2fa28ee
Julian <julianthant@gmail.com>
2024-04-24 20:55:11 -0700

    Refactor AddAddressForm component to use react-toastify for success and error messages Refactor PaymentComponent to handle success parameter in useEffect Refactor AddressComponent to handle success parameter in useEffect Refactor QueryProvider in providers.tsx to use staleTime: Infinity for queries Refactor and remove unused code in settings pages Add .sentryclirc to .gitignore Refactor layout.tsx to add Suspense component for improved loading experience Refactor layout.tsx to remove Suspense component and improve code organization Refactor layout.tsx to remove Suspense component and improve code organization Refactor QueryProvider in providers.tsx to use useState for queryClient initialization

--- src/app/(main)/(pages)/settings/forms/address/_components/UpdateAddressForm.tsx
+++ src/app/(main)/(pages)/settings/forms/address/_components/UpdateAddressForm.tsx
@@ -7,6 +7,7 @@ import { useForm } from 'react-hook-form';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useTransition } from 'react';
 
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { addressSchema } from '@/schemas';
 import { UpdateAddress } from '@/server/actions/accountAddresses/update-address';
@@ -43,6 +44,7 @@ import {
   SelectValue,
 } from '@/components/ui/select';
 import { toast } from 'react-toastify';
+import { Spinner } from '@nextui-org/react';
 
 interface CountryEditProps {
   countryNames: string[];
@@ -54,44 +56,54 @@ export function UpdateAddressForm({ countryNames, states }: CountryEditProps) {
 
   const router = useRouter();
   const params = useSearchParams();
+  const queryClient = useQueryClient();
 
   const id = params.get('address-id');
 
+  const { data: address, isLoading } = useQuery({
+    queryFn: () =>
+      axios.get(`/api/addresses/getUnique/${id}`).then((res) => res.data),
+    queryKey: ['address', { addressId: id }],
+  });
+
   const form = useForm<z.infer<typeof addressSchema>>({
     resolver: zodResolver(addressSchema),
-    defaultValues: async () => {
-      const address = await axios
-        .get(`/api/addresses/getUnique/${id}`)
-        .then((res) => res.data);
+    values: {
+      country: address?.country,
+      fullName: address?.fullName || '',
+      phoneNumber: address?.phoneNumber || '',
+      streetAddress: address?.streetAddress || '',
+      streetOptional: address?.streetOptional || '',
+      city: address?.city || '',
+      states: address?.states || '',
+      zipCode: address?.zipCode || '',
+      deliveryInstructions: address?.deliveryInstructions || '',
+      defaultAddress: address?.defaultAddress || false,
+    },
+  });
 
-      return {
-        country: address.country || '',
-        fullName: address.fullName || '',
-        phoneNumber: address.phoneNumber || '',
-        streetAddress: address.streetAddress || '',
-        streetOptional: address.streetOptional || '',
-        city: address.city || '',
-        states: address.states || '',
-        zipCode: address.zipCode || '',
-        deliveryInstructions: address.deliveryInstructions || '',
-        defaultAddress: address.defaultAddress || false,
-      };
+  const { mutateAsync: updateAddress } = useMutation({
+    mutationFn: ({ values, id }: { values: any; id: string }) =>
+      UpdateAddress(values, id),
+    onSuccess: (data) => {
+      if (data.success) {
+        queryClient.invalidateQueries({
+          queryKey: ['address', { addressId: id }],
+        });
+        toast.success(data.success);
+        router.push('/settings/account/addresses?success=true');
+      }
+
+      if (data.error) {
+        toast.error(data.error);
+        router.push('/settings/account/addresses');
+      }
     },
   });
 
   const onSubmit = (values: z.infer<typeof addressSchema>) => {
     startTransition(() => {
-      UpdateAddress(values, id as string).then((data) => {
-        if (data.success) {
-          toast.success(data.success);
-          router.push('/settings/account/addresses');
-        }
-
-        if (data.error) {
-          toast.error(data.error);
-          router.push('/settings/account/addresses');
-        }
-      });
+      updateAddress({ values, id: id as string });
     });
   };
 
@@ -101,171 +113,195 @@ export function UpdateAddressForm({ countryNames, states }: CountryEditProps) {
         <CardTitle>Update Address</CardTitle>
         <CardDescription>Edit your existing address.</CardDescription>
       </CardHeader>
-      <CardContent className="grid gap-6">
-        <Form {...form}>
-          <form className="space-y-3" onSubmit={form.handleSubmit(onSubmit)}>
-            <FormField
-              control={form.control}
-              name="country"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel htmlFor="country">Country/Region</FormLabel>
-
-                  <Select
-                    onValueChange={field.onChange}
-                    value={field.value}
-                    disabled={isPending}
-                  >
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select your country" />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      {countryNames.sort().map((country) => (
-                        <SelectItem value={country} key={country}>
-                          {country}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="fullName"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel htmlFor="name">Full Name</FormLabel>
-                  <Input
-                    id="fullName"
-                    placeholder="First and Last name"
-                    onChange={field.onChange}
-                    defaultValue={field.value}
-                    disabled={isPending}
-                  />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="phoneNumber"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel htmlFor="number">Phone Number</FormLabel>
-                  <Input
-                    id="phoneNumber"
-                    onChange={field.onChange}
-                    defaultValue={field.value}
-                    disabled={isPending}
-                  />
-                </FormItem>
-              )}
-            />
-
-            <div className="grid gap-2">
+      <CardContent className="grid gap-6 relative">
+        {!isLoading ? (
+          <Form {...form}>
+            <form className="space-y-3" onSubmit={form.handleSubmit(onSubmit)}>
               <FormField
                 control={form.control}
-                name="streetAddress"
+                name="country"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel htmlFor="address">Address</FormLabel>
-                    <Input
-                      id="streetAddress"
-                      placeholder="Street address or P.O. Box"
-                      onChange={field.onChange}
+                    <FormLabel htmlFor="country">Country/Region</FormLabel>
+
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={address?.country}
+                      value={field.value}
                       disabled={isPending}
-                      defaultValue={field.value}
-                    />
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select your country" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {countryNames.sort().map((country) => (
+                          <SelectItem value={country} key={country}>
+                            {country}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+
+                    <FormMessage />
                   </FormItem>
                 )}
               />
 
               <FormField
                 control={form.control}
-                name="streetOptional"
+                name="fullName"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel className="sr-only" htmlFor="address">
-                      Optional Address
-                    </FormLabel>
+                    <FormLabel htmlFor="name">Full Name</FormLabel>
                     <Input
-                      id="streetOptional"
-                      placeholder="Apt, suite, unit, building, floor, etc."
+                      id="fullName"
+                      placeholder="First and Last name"
                       onChange={field.onChange}
-                      disabled={isPending}
                       defaultValue={field.value}
+                      disabled={isPending}
                     />
                   </FormItem>
                 )}
               />
-            </div>
 
-            <div className="flex justify-between w-full gap-2">
               <FormField
                 control={form.control}
-                name="city"
+                name="phoneNumber"
                 render={({ field }) => (
-                  <FormItem className="w-full">
-                    <FormLabel htmlFor="city">City</FormLabel>
+                  <FormItem>
+                    <FormLabel htmlFor="number">Phone Number</FormLabel>
                     <Input
-                      id="city"
+                      id="phoneNumber"
                       onChange={field.onChange}
-                      disabled={isPending}
                       defaultValue={field.value}
+                      disabled={isPending}
                     />
                   </FormItem>
                 )}
               />
 
-              {form.watch('country') === 'United States' && (
+              <div className="grid gap-2">
                 <FormField
                   control={form.control}
-                  name="states"
+                  name="streetAddress"
                   render={({ field }) => (
-                    <FormItem className="w-full">
-                      <FormLabel htmlFor="states">States</FormLabel>
-
-                      <Select
-                        onValueChange={field.onChange}
-                        value={field.value}
-                      >
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          <SelectGroup>
-                            <SelectLabel>Countries</SelectLabel>
-                            {states.sort().map((state) => (
-                              <SelectItem value={state} key={state}>
-                                {state}
-                              </SelectItem>
-                            ))}
-                          </SelectGroup>
-                        </SelectContent>
-                      </Select>
-
-                      <FormMessage />
+                    <FormItem>
+                      <FormLabel htmlFor="address">Address</FormLabel>
+                      <Input
+                        id="streetAddress"
+                        placeholder="Street address or P.O. Box"
+                        onChange={field.onChange}
+                        disabled={isPending}
+                        defaultValue={field.value}
+                      />
                     </FormItem>
                   )}
                 />
-              )}
+
+                <FormField
+                  control={form.control}
+                  name="streetOptional"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="sr-only" htmlFor="address">
+                        Optional Address
+                      </FormLabel>
+                      <Input
+                        id="streetOptional"
+                        placeholder="Apt, suite, unit, building, floor, etc."
+                        onChange={field.onChange}
+                        disabled={isPending}
+                        defaultValue={field.value}
+                      />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <div className="flex justify-between w-full gap-2">
+                <FormField
+                  control={form.control}
+                  name="city"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel htmlFor="city">City</FormLabel>
+                      <Input
+                        id="city"
+                        onChange={field.onChange}
+                        disabled={isPending}
+                        defaultValue={field.value}
+                      />
+                    </FormItem>
+                  )}
+                />
+
+                {form.watch('country') === 'United States' && (
+                  <FormField
+                    control={form.control}
+                    name="states"
+                    render={({ field }) => (
+                      <FormItem className="w-full">
+                        <FormLabel htmlFor="states">States</FormLabel>
+
+                        <Select
+                          onValueChange={field.onChange}
+                          value={field.value}
+                          defaultValue={address?.country}
+                          disabled={isPending}
+                        >
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            <SelectGroup>
+                              <SelectLabel>Countries</SelectLabel>
+                              {states.sort().map((state) => (
+                                <SelectItem value={state} key={state}>
+                                  {state}
+                                </SelectItem>
+                              ))}
+                            </SelectGroup>
+                          </SelectContent>
+                        </Select>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                )}
+
+                <FormField
+                  control={form.control}
+                  name="zipCode"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel htmlFor="zipcode">ZIP Code</FormLabel>
+                      <Input
+                        id="zipCode"
+                        onChange={field.onChange}
+                        defaultValue={field.value}
+                        disabled={isPending}
+                      />
+                    </FormItem>
+                  )}
+                />
+              </div>
 
               <FormField
                 control={form.control}
-                name="zipCode"
+                name="deliveryInstructions"
                 render={({ field }) => (
-                  <FormItem className="w-full">
-                    <FormLabel htmlFor="zipcode">ZIP Code</FormLabel>
+                  <FormItem>
+                    <FormLabel htmlFor="zipcode">
+                      Delivery Instructions (optional)
+                    </FormLabel>
                     <Input
-                      id="zipCode"
+                      id="deliveryInstructions"
+                      placeholder="Add preferences, notes, access codes and more"
                       onChange={field.onChange}
                       defaultValue={field.value}
                       disabled={isPending}
@@ -273,66 +309,49 @@ export function UpdateAddressForm({ countryNames, states }: CountryEditProps) {
                   </FormItem>
                 )}
               />
-            </div>
 
-            <FormField
-              control={form.control}
-              name="deliveryInstructions"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel htmlFor="zipcode">
-                    Delivery Instructions (optional)
-                  </FormLabel>
-                  <Input
-                    id="deliveryInstructions"
-                    placeholder="Add preferences, notes, access codes and more"
-                    onChange={field.onChange}
-                    defaultValue={field.value}
-                    disabled={isPending}
-                  />
-                </FormItem>
-              )}
-            />
+              <FormField
+                control={form.control}
+                name="defaultAddress"
+                render={({ field }) => (
+                  <FormItem className="flex items-center gap-2 pb-1">
+                    <FormLabel className="sr-only" htmlFor="default-payment">
+                      Default Address
+                    </FormLabel>
+                    <FormControl>
+                      <Checkbox
+                        className="mr-0"
+                        id="defaultAddress"
+                        onCheckedChange={field.onChange}
+                        disabled={isPending}
+                        defaultChecked={field.value}
+                        checked={field.value}
+                      />
+                    </FormControl>
 
-            <FormField
-              control={form.control}
-              name="defaultAddress"
-              render={({ field }) => (
-                <FormItem className="flex items-center gap-2 pb-1">
-                  <FormLabel className="sr-only" htmlFor="default-payment">
-                    Default Address
-                  </FormLabel>
-                  <FormControl>
-                    <Checkbox
-                      className="mr-0"
-                      id="defaultAddress"
-                      onCheckedChange={field.onChange}
-                      disabled={isPending}
-                      defaultChecked={field.value}
-                      checked={field.value}
-                    />
-                  </FormControl>
-
-                  <label
-                    htmlFor="terms"
-                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                  >
-                    Make this my default address
-                  </label>
-                </FormItem>
-              )}
-            />
-
-            <div className="pt-1">
-              <Button disabled={isPending} className="w-full" type="submit">
-                {isPending && (
-                  <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                    <label
+                      htmlFor="terms"
+                      className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    >
+                      Make this my default address
+                    </label>
+                  </FormItem>
                 )}
-                Update Address
-              </Button>
-            </div>
-          </form>
-        </Form>
+              />
+
+              <div className="pt-1">
+                <Button disabled={isPending} className="w-full" type="submit">
+                  {isPending && (
+                    <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                  )}
+                  Update Address
+                </Button>
+              </div>
+            </form>
+          </Form>
+        ) : (
+          <Spinner color="primary" className="my-4" />
+        )}
       </CardContent>
     </Card>
   );
