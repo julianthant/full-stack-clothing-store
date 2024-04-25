a8cd0f588441cf1c71db735a4c8a49452a33d8c7
Julian <julianthant@gmail.com>
2024-04-24 23:11:37 -0700

    Add prefetch attribute to Login and Register links in LoginForm.tsx

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -93,6 +93,7 @@ export function LoginForm({ className, ...props }: LoginFormProps) {
         <Link
           href="/auth/login"
           className={cn('absolute right-4 top-4 md:right-8 md:top-8')}
+          prefetch
         >
           Login
         </Link>
@@ -100,6 +101,7 @@ export function LoginForm({ className, ...props }: LoginFormProps) {
         <Link
           href="/auth/register"
           className={cn('absolute right-4 top-4 md:right-8 md:top-8')}
+          prefetch
         >
           Register
         </Link>
