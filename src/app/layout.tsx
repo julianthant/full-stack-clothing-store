ff3e44979096ecc1b8a4a72e205ce4e543b8d175
Julian <julianthant@gmail.com>
2024-04-24 17:11:08 -0700

    Refactor layout.tsx to add Suspense component for improved loading experience

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { satoshi } from '@/lib/fonts';
 import './globals.css';
 import { Providers, QueryProvider } from './providers';
 import 'react-toastify/dist/ReactToastify.css';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
   title: 'Clothes.co',
@@ -18,7 +19,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`antialiased ${satoshi.className}`}>
         <Providers>
-          <QueryProvider>{children}</QueryProvider>
+          <Suspense fallback={null}>
+            <QueryProvider>{children}</QueryProvider>
+          </Suspense>
         </Providers>
       </body>
     </html>
