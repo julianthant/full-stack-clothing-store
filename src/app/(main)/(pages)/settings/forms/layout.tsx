a10a1f6acfbf9b3cf252fb1dce22e6830d5c0c58
Julian <julianthant@gmail.com>
2024-04-24 23:11:29 -0700

    Refactor layout.tsx to improve code organization and add container class

--- src/app/(main)/(pages)/settings/forms/layout.tsx
+++ src/app/(main)/(pages)/settings/forms/layout.tsx
@@ -6,9 +6,11 @@ interface LayoutProps {
 
 const Layout: FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="mx-auto w-fit min-h-[300px]">
-      <div className="grid space-y-3 border p-10 rounded-3xl mt-6">
-        <div className="flex items-center justify-center">{children}</div>
+    <div className="container">
+      <div className="mx-auto min-h-[300px]">
+        <div className="grid space-y-3 border sm:p-10 rounded-3xl mt-6">
+          <div className="flex items-center justify-center">{children}</div>
+        </div>
       </div>
     </div>
   );
