ee99deec4c0c9d4292c49729082655397d0067ab
Julian <julianthant@gmail.com>
2024-04-25 19:45:05 -0700

    Refactor AddressComponent to improve loading experience and add new address button

--- src/app/(main)/(pages)/settings/(setting-pages)/account/addresses/_components/AdresssComponent.tsx
+++ src/app/(main)/(pages)/settings/(setting-pages)/account/addresses/_components/AdresssComponent.tsx
@@ -4,14 +4,16 @@ import Link from 'next/link';
 import axios from 'axios';
 import dynamic from 'next/dynamic';
 
-import { useEffect, Suspense } from 'react';
+import { cn } from '@/lib/utils';
+import { useEffect, useState } from 'react';
 import { useCurrentUser } from '@/hooks/useCurrentUser';
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams, useRouter, usePathname } from 'next/navigation';
 import { useQuery } from '@tanstack/react-query';
 
 import { PlusIcon } from 'lucide-react';
 import { Card, CardContent } from '@/components/ui/card';
 import { AddressComponentSkeleton } from '@/components/skeleton/AddressComponentSkeleton';
+import { Button } from '@/components/ui/button';
 
 const AddressCards = dynamic(() =>
   import('./AddressCards').then((mod) => mod.AddressCards)
@@ -24,10 +26,13 @@ export const AddressComponent = () => {
   const params = useSearchParams();
   const success = params.get('success');
 
+  const router = useRouter();
+  const pathname = usePathname();
+
   const {
     data: addresses,
     refetch,
-    isPending,
+    isFetched,
   } = useQuery({
     queryFn: () =>
       axios.get(`/api/addresses/getAll/${userId}`).then((res) => res.data),
@@ -39,13 +44,14 @@ export const AddressComponent = () => {
   useEffect(() => {
     if (success) {
       refetch();
+      router.replace(pathname);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [success]);
 
   return (
-    <div className="grid gap-4 relative 2xl:grid-cols-4 xl:grid-cols-3 lg:grid-cols-2 dark:bg-black">
-      <Card className="rounded-lg p-4 min-h-[319px]">
+    <div className="lg:grid flex flex-col gap-4 relative 2xl:grid-cols-4 xl:grid-cols-3 lg:grid-cols-2 dark:bg-black max-lg:min-h-[700px]">
+      <Card className="rounded-lg p-4 h-[319px]">
         <Link href="/settings/forms/address/add-new-address">
           <CardContent className="hover:bg-foreground-200 bg-foreground-100 cursor-pointer flex flex-col items-center justify-center p-0 rounded-lg h-full border-2 border-dashed border-foreground-400">
             <PlusIcon size={70} className="text-foreground-400 mx-auto" />
@@ -58,13 +64,11 @@ export const AddressComponent = () => {
         </Link>
       </Card>
 
-      <Suspense fallback={<AddressComponentSkeleton />}>
-        {isPending ? (
-          <AddressComponentSkeleton />
-        ) : (
-          <AddressCards addresses={addresses} />
-        )}
-      </Suspense>
+      {!isFetched ? (
+        <AddressComponentSkeleton />
+      ) : (
+        <AddressCards addresses={addresses} />
+      )}
     </div>
   );
 };
