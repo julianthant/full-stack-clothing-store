15a3100f0f7b5e7504ac2f9a92f1bd3e5fbfd08c
Julian <julianthant@gmail.com>
2024-04-26 08:32:58 -0700

    Refactor dynamic imports for Footer and ToastContainer components in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import './globals.css';
 import type { Metadata } from 'next';
 
 import { Satoshi } from './fonts/fonts';
-import { Suspense } from 'react';
 import { Providers, QueryProvider } from './providers';
 
 export const metadata: Metadata = {
@@ -25,9 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`antialiased ${Satoshi.className}`}>
         <Providers>
-          <QueryProvider>
-            <Suspense fallback={null}>{children}</Suspense>
-          </QueryProvider>
+          <QueryProvider>{children}</QueryProvider>
         </Providers>
       </body>
     </html>
