212863e25117dae16c26b856374f08cab825288c
Julian <julianthant@gmail.com>
2024-04-29 02:05:21 -0700

    Fix expiry date formatting issue in AddPaymentMethodDrawer component

--- src/app/(main)/settings/billing/_components/AddPaymentMethodDrawer.tsx
+++ src/app/(main)/settings/billing/_components/AddPaymentMethodDrawer.tsx
@@ -211,6 +211,7 @@ export function AddPaymentMethodDrawer({ user, open, setOpen }: any) {
                         })}
                         onInput={expiryDateFormat}
                         maxLength={7}
+                        onKeyDown={expiryDateFormatDeleted}
                       />
                     </FormControl>
 
